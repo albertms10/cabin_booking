42ee92a29192a3272422ae8bee610433e5d2e3c2
albert <albertmasa2@gmail.com>
2020-11-14 20:15:36 +0100

    feat(booking): add EmptyBooking

---
+++ lib/widgets/layout/empty_booking.dart
@@ -0,0 +1,35 @@
+import 'package:cabin_booking/constants.dart';
+import 'package:flutter/material.dart';
+
+class EmptyBooking extends StatelessWidget {
+  final int duration;
+
+  EmptyBooking({this.duration = 60});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Container(
+        margin: EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          color: Color.fromARGB(10, 0, 0, 0),
+          borderRadius: BorderRadius.all(Radius.circular(4)),
+        ),
+        child: Tooltip(
+          message: '$duration min',
+          child: InkWell(
+            borderRadius: BorderRadius.all(Radius.circular(4)),
+            onTap: () {},
+            child: Icon(
+              Icons.add,
+              size: 18,
+              color: Colors.black54,
+            ),
+          ),
+        ),
+      ),
+      width: double.infinity,
+      height: duration * bookingHeightRatio,
+    );
+  }
+}
