f8208a179e8cbef3f56d0137edb64af0dd067711
albert <albertmasa2@gmail.com>
2020-11-14 21:12:16 +0100

    feat(current_time_builder): wrap with periodic builder

--- lib/widgets/layout/current_time_indicator.dart
+++ lib/widgets/layout/current_time_indicator.dart
@@ -1,48 +1,54 @@
 import 'package:cabin_booking/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:timer_builder/timer_builder.dart';
 
 class CurrentTimeIndicator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    int _difference = DateTime.now()
-        .difference(DateTime.parse(
-            DateFormat('yyyy-MM-dd').format(DateTime.now()) + ' 15:00'))
-        .inMinutes;
+    return TimerBuilder.periodic(
+      Duration(seconds: 5),
+      builder: (context) {
+        int _difference = DateTime.now()
+            .difference(DateTime.parse(
+                DateFormat('yyyy-MM-dd').format(DateTime.now()) + ' 15:00'))
+            .inMinutes;
 
-    return _difference > 0
-        ? Column(
-            children: [
-              SizedBox(
-                height: _difference * bookingHeightRatio,
-                child: Container(
-                  alignment: Alignment.bottomLeft,
-                  padding: EdgeInsets.all(8),
-                  child: Text(
-                    DateFormat('HH:mm').format(DateTime.now()),
-                    style: TextStyle(
-                      color: Colors.red[400],
-                      fontWeight: FontWeight.bold,
+        return _difference > 0
+            ? Column(
+                children: [
+                  SizedBox(
+                    height: _difference * bookingHeightRatio,
+                    child: Container(
+                      alignment: Alignment.bottomLeft,
+                      padding: EdgeInsets.all(8),
+                      child: Text(
+                        DateFormat('HH:mm').format(DateTime.now()),
+                        style: TextStyle(
+                          color: Colors.red[400],
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-              Container(
-                height: 3,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Colors.red[400],
-                  boxShadow: [
-                    const BoxShadow(
-                      color: Colors.black12,
-                      blurRadius: 3,
-                      offset: Offset(3, 4),
+                  Container(
+                    height: 2,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.red[400],
+                      boxShadow: [
+                        const BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 3,
+                          offset: Offset(3, 4),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-              ),
-            ],
-          )
-        : Container();
+                  ),
+                ],
+              )
+            : Container();
+      },
+    );
   }
 }
