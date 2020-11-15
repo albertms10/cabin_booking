526851970356e25d002beef7cec4953ecb875d59
albert <albertmasa2@gmail.com>
2020-11-15 14:00:43 +0100

    feat(constants): add maxSlotDuration

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
 const double bookingHeightRatio = 2.2;
+
+const Duration maxSlotDuration = const Duration(minutes: 60);
+
 const TimeOfDay timeTableStartTime = const TimeOfDay(hour: 15, minute: 0);
 const TimeOfDay timeTableEndTime = const TimeOfDay(hour: 22, minute: 0);
