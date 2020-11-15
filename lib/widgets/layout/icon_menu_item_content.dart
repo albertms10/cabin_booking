85417e80a958c3dfd5d0ff0e6d387f85b2249a39
albert <albertmasa2@gmail.com>
2020-11-15 20:13:31 +0100

    feat(icon_menu_item_content): extract Widget

---
+++ lib/widgets/layout/icon_menu_item_content.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class IconMenuItemContent extends StatelessWidget {
+  final String text;
+  final IconData icon;
+
+  IconMenuItemContent({@required this.text, this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(
+          icon,
+          size: 20,
+          color: Colors.black54,
+        ),
+        const SizedBox(width: 14),
+        Text(
+          text,
+          style: const TextStyle(fontSize: 14),
+        ),
+      ],
+    );
+  }
+}
