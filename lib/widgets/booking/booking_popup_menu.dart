56af52eada9ccb0de26019b0c4276b6c9828ae2a
albert <albertmasa2@gmail.com>
2020-11-17 13:35:21 +0100

    feat(booking_popup_menu): improve menu button display

--- lib/widgets/booking/booking_popup_menu.dart
+++ lib/widgets/booking/booking_popup_menu.dart
@@ -19,60 +19,70 @@ class BookingPopupMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PopupMenuButton<String>(
-      onSelected: (choice) async {
-        final cabinManager = Provider.of<CabinManager>(context, listen: false);
-
-        switch (choice) {
-          case 'edit':
-            final _booking = await showDialog<Booking>(
-              context: context,
-              builder: (context) => BookingDialog(
-                booking..cabinId = cabin.id,
-              ),
-            );
-
-            if (_booking != null)
-              cabinManager.modifyBooking(cabin.id, _booking);
-            break;
-
-          case 'delete':
-            final _shallDelete = await showDialog<bool>(
-              context: context,
-              builder: (context) => DeleteBookingDialog(),
-            );
-
-            if (_shallDelete)
-              cabinManager.removeBookingById(cabin.id, booking.id);
-
-            break;
-        }
-      },
-      icon: const Icon(
-        Icons.more_vert,
-        size: 16,
-        color: Colors.black54,
-      ),
-      itemBuilder: (BuildContext context) {
-        return [
-          PopupMenuItem(
-            value: 'edit',
-            child: IconMenuItemContent(
-              text: AppLocalizations.of(context).edit,
-              icon: Icons.edit,
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(24),
+      child: Material(
+        color: Colors.transparent,
+        child: PopupMenuButton<String>(
+          child: Container(
+            padding: const EdgeInsets.all(4),
+            child: const Icon(
+              Icons.more_vert,
+              size: 18,
+              color: Colors.black54,
             ),
-            height: 41,
           ),
-          PopupMenuItem(
-            value: 'delete',
-            child: IconMenuItemContent(
-              text: MaterialLocalizations.of(context).deleteButtonTooltip,
-              icon: Icons.delete,
-            ),
-            height: 41,
-          )
-        ];
-      },
+          onSelected: (choice) async {
+            final cabinManager =
+                Provider.of<CabinManager>(context, listen: false);
+
+            switch (choice) {
+              case 'edit':
+                final _booking = await showDialog<Booking>(
+                  context: context,
+                  builder: (context) => BookingDialog(
+                    booking..cabinId = cabin.id,
+                  ),
+                );
+
+                if (_booking != null)
+                  cabinManager.modifyBooking(cabin.id, _booking);
+                break;
+
+              case 'delete':
+                final _shallDelete = await showDialog<bool>(
+                  context: context,
+                  builder: (context) => DeleteBookingDialog(),
+                );
+
+                if (_shallDelete)
+                  cabinManager.removeBookingById(cabin.id, booking.id);
+
+                break;
+            }
+          },
+          itemBuilder: (BuildContext context) {
+            return [
+              PopupMenuItem(
+                value: 'edit',
+                child: IconMenuItemContent(
+                  text: AppLocalizations.of(context).edit,
+                  icon: Icons.edit,
+                ),
+                height: 41,
+              ),
+              PopupMenuItem(
+                value: 'delete',
+                child: IconMenuItemContent(
+                  text: MaterialLocalizations.of(context).deleteButtonTooltip,
+                  icon: Icons.delete,
+                ),
+                height: 41,
+              )
+            ];
+          },
+        ),
+      ),
     );
   }
 }
