bacafe3d5a25bd4acfda628ac49d6249a3940d98
albert <albertmasa2@gmail.com>
2020-11-21 11:38:45 +0100

    style(booking_floating_action_butto): fix indentation

--- lib/widgets/booking/booking_floating_action_button.dart
+++ lib/widgets/booking/booking_floating_action_button.dart
@@ -13,48 +13,49 @@ class BookingFloatingActionButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer2<DayHandler, CabinManager>(
-        builder: (context, dayHandler, cabinManager, child) {
-      return FloatingActionButtonMenu(
-        marginBottom: 24,
-        marginRight: 24,
-        animatedIcon: AnimatedIcons.add_event,
-        animatedIconTheme: IconThemeData(size: 28),
-        label: AppLocalizations.of(context).booking,
-        onPress: () async {
-          final _booking = await showDialog<Booking>(
-            context: context,
-            builder: (context) => BookingDialog(
-              Booking(
-                dateStart: tryParseDateTimeWithFormattedTimeOfDay(
-                  dateTime: dayHandler.dateTime,
-                  formattedTimeOfDay: timeTableStartTime.format(context),
+      builder: (context, dayHandler, cabinManager, child) {
+        return FloatingActionButtonMenu(
+          marginBottom: 24,
+          marginRight: 24,
+          animatedIcon: AnimatedIcons.add_event,
+          animatedIconTheme: IconThemeData(size: 28),
+          label: AppLocalizations.of(context).booking,
+          onPress: () async {
+            final _booking = await showDialog<Booking>(
+              context: context,
+              builder: (context) => BookingDialog(
+                Booking(
+                  dateStart: tryParseDateTimeWithFormattedTimeOfDay(
+                    dateTime: dayHandler.dateTime,
+                    formattedTimeOfDay: timeTableStartTime.format(context),
+                  ),
+                  dateEnd: tryParseDateTimeWithFormattedTimeOfDay(
+                    dateTime: dayHandler.dateTime,
+                    formattedTimeOfDay: TimeOfDay(
+                      hour: timeTableStartTime.hour + 1,
+                      minute: timeTableStartTime.minute,
+                    ).format(context),
+                  ),
+                  cabinId: cabinManager.cabins.first.id,
                 ),
-                dateEnd: tryParseDateTimeWithFormattedTimeOfDay(
-                  dateTime: dayHandler.dateTime,
-                  formattedTimeOfDay: TimeOfDay(
-                    hour: timeTableStartTime.hour + 1,
-                    minute: timeTableStartTime.minute,
-                  ).format(context),
-                ),
-                cabinId: cabinManager.cabins.first.id,
               ),
-            ),
-          );
+            );
 
-          if (_booking != null)
-            cabinManager.addBooking(_booking.cabinId, _booking);
-        },
-        children: [
-          FloatingActionButtonMenuChild(
-            icon: Icons.repeat,
-            label: AppLocalizations.of(context).recurringBooking,
-          ),
-          FloatingActionButtonMenuChild(
-            icon: Icons.lock,
-            label: AppLocalizations.of(context).lockedRange,
-          ),
-        ],
-      );
-    });
+            if (_booking != null)
+              cabinManager.addBooking(_booking.cabinId, _booking);
+          },
+          children: [
+            FloatingActionButtonMenuChild(
+              icon: Icons.repeat,
+              label: AppLocalizations.of(context).recurringBooking,
+            ),
+            FloatingActionButtonMenuChild(
+              icon: Icons.lock,
+              label: AppLocalizations.of(context).lockedRange,
+            ),
+          ],
+        );
+      },
+    );
   }
 }
