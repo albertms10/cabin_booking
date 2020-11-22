21d55fa8fcd7d90e3a6ee8aaef94f95c3f248f0c
albert <albertmasa2@gmail.com>
2020-11-22 22:29:05 +0100

    feat(cabins_row): flatten background

--- lib/widgets/cabin/cabins_row.dart
+++ lib/widgets/cabin/cabins_row.dart
@@ -7,26 +7,23 @@ import 'package:provider/provider.dart';
 class CabinsRow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Material(
-      elevation: 2.0,
-      color: Colors.white,
-      child: Container(
-        padding: const EdgeInsets.symmetric(vertical: 24.0),
-        child: Consumer<CabinManager>(
-          builder: (context, cabinManager, child) {
-            return Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                child,
-                for (Cabin cabin in cabinManager.cabins)
-                  Expanded(
-                    child: CabinIcon(number: cabin.number),
-                  ),
-              ],
-            );
-          },
-          child: const SizedBox(width: 180.0),
-        ),
+    return Container(
+      color: Colors.grey[50],
+      padding: const EdgeInsets.symmetric(vertical: 24.0),
+      child: Consumer<CabinManager>(
+        builder: (context, cabinManager, child) {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              child,
+              for (Cabin cabin in cabinManager.cabins)
+                Expanded(
+                  child: CabinIcon(number: cabin.number),
+                ),
+            ],
+          );
+        },
+        child: const SizedBox(width: 180.0),
       ),
     );
   }
