ae355972dc2ec11a8c55655b87005bca854c57e2
albert <albertmasa2@gmail.com>
2020-11-25 23:27:00 +0100

    fix(constants): lower minSlotDuration

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 const double bookingHeightRatio = 2.2;
 
-const Duration minSlotDuration = Duration(minutes: 30);
+const Duration minSlotDuration = Duration(minutes: 20);
 const Duration maxSlotDuration = Duration(minutes: 60);
 
 const TimeOfDay timeTableStartTime = TimeOfDay(hour: 15, minute: 0);
