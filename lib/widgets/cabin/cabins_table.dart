e01421f9462c91a03262879402bc7c43521369d3
albert <albertmasa2@gmail.com>
2020-11-26 17:19:02 +0100

    feat(cabins_table): add selection action bar
    * implement cabin removal

--- lib/widgets/cabin/cabins_table.dart
+++ lib/widgets/cabin/cabins_table.dart
@@ -1,8 +1,11 @@
 import 'package:cabin_booking/l10n/app_localizations.dart';
+import 'package:cabin_booking/model/cabin_manager.dart';
 import 'package:cabin_booking/widgets/cabin/cabin_icon.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CabinTableRow {
+  final String id;
   final int number;
   final int bookingsCount;
   final int recurringBookingsCount;
@@ -10,6 +13,7 @@ class CabinTableRow {
   bool selected;
 
   CabinTableRow({
+    this.id,
     this.number,
     this.bookingsCount,
     this.recurringBookingsCount,
@@ -28,6 +32,32 @@ class CabinsTable extends StatefulWidget {
 
   @override
   _CabinsTableState createState() => _CabinsTableState();
+
+  int get selectedItems {
+    var count = 0;
+
+    for (final cabin in cabinRows) {
+      if (cabin.selected) count++;
+    }
+
+    return count;
+  }
+
+  void unselect() {
+    for (final cabin in cabinRows) {
+      if (cabin.selected) cabin.selected = false;
+    }
+  }
+
+  void removeSelected(BuildContext context) {
+    final removableIds = cabinRows
+        .where((cabin) => cabin.selected)
+        .map((cabin) => cabin.id)
+        .toList();
+
+    Provider.of<CabinManager>(context, listen: false)
+        .removeCabinsByIds(removableIds);
+  }
 }
 
 typedef _OnSortFunction = Function(bool ascending);
@@ -96,94 +126,136 @@ class _CabinsTableState extends State<CabinsTable> {
 
   @override
   Widget build(BuildContext context) {
-    return DataTable(
-      dataRowHeight: 82.0,
-      showCheckboxColumn: true,
-      sortAscending: _sortAscending,
-      sortColumnIndex: _sortColumnIndex,
-      columns: [
-        DataColumn(
-          label: Padding(
-            padding: const EdgeInsets.only(right: 8.0),
-            child: Text(AppLocalizations.of(context).cabin),
-          ),
-          onSort: onSortColumn,
-        ),
-        DataColumn(
-          label: Padding(
-            padding: const EdgeInsets.only(left: 8.0),
-            child: Text(AppLocalizations.of(context).bookings),
-          ),
-          numeric: true,
-          onSort: onSortColumn,
-        ),
-        DataColumn(
-          label: Padding(
-            padding: const EdgeInsets.only(left: 8.0),
-            child: Text(AppLocalizations.of(context).recurringBookings),
-          ),
-          numeric: true,
-          onSort: onSortColumn,
-        ),
-        DataColumn(
-          label: Padding(
-            padding: const EdgeInsets.only(left: 8.0),
-            child: Text(AppLocalizations.of(context).occupancyRate),
-          ),
-          numeric: true,
-          onSort: onSortColumn,
-        ),
-      ],
-      rows: List<DataRow>.generate(
-        widget.cabinRows.length,
-        (index) {
-          final cabinRow = widget.cabinRows[index];
-
-          return DataRow(
-            selected: widget.cabinRows[index].selected,
-            onSelectChanged: (selected) {
-              setState(() => widget.cabinRows[index].selected = selected);
-            },
-            cells: [
-              DataCell(
-                CabinIcon(number: cabinRow.number),
-              ),
-              DataCell(
-                Text(
-                  '${cabinRow.bookingsCount}',
-                  style: Theme.of(context).textTheme.headline5,
-                ),
-              ),
-              DataCell(
-                Text(
-                  '${cabinRow.recurringBookingsCount}',
-                  style: Theme.of(context).textTheme.headline5,
-                ),
-              ),
-              DataCell(
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      '${(cabinRow.occupancyRate * 100).round()}',
-                      style: Theme.of(context).textTheme.headline5,
+    return Stack(
+      children: [
+        ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(54.0),
+              child: DataTable(
+                dataRowHeight: 82.0,
+                showCheckboxColumn: true,
+                sortAscending: _sortAscending,
+                sortColumnIndex: _sortColumnIndex,
+                columns: [
+                  DataColumn(
+                    label: Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: Text(AppLocalizations.of(context).cabin),
                     ),
-                    const SizedBox(width: 2),
-                    Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        const SizedBox(height: 2),
-                        Text('%', style: Theme.of(context).textTheme.subtitle2)
+                    onSort: onSortColumn,
+                  ),
+                  DataColumn(
+                    label: Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Text(AppLocalizations.of(context).bookings),
+                    ),
+                    numeric: true,
+                    onSort: onSortColumn,
+                  ),
+                  DataColumn(
+                    label: Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child:
+                          Text(AppLocalizations.of(context).recurringBookings),
+                    ),
+                    numeric: true,
+                    onSort: onSortColumn,
+                  ),
+                  DataColumn(
+                    label: Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Text(AppLocalizations.of(context).occupancyRate),
+                    ),
+                    numeric: true,
+                    onSort: onSortColumn,
+                  ),
+                ],
+                rows: List<DataRow>.generate(
+                  widget.cabinRows.length,
+                  (index) {
+                    final cabinRow = widget.cabinRows[index];
+
+                    return DataRow(
+                      selected: widget.cabinRows[index].selected,
+                      onSelectChanged: (selected) {
+                        setState(
+                            () => widget.cabinRows[index].selected = selected);
+                      },
+                      cells: [
+                        DataCell(
+                          CabinIcon(number: cabinRow.number),
+                        ),
+                        DataCell(
+                          Text(
+                            '${cabinRow.bookingsCount}',
+                            style: Theme.of(context).textTheme.headline5,
+                          ),
+                        ),
+                        DataCell(
+                          Text(
+                            '${cabinRow.recurringBookingsCount}',
+                            style: Theme.of(context).textTheme.headline5,
+                          ),
+                        ),
+                        DataCell(
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Text(
+                                '${(cabinRow.occupancyRate * 100).round()}',
+                                style: Theme.of(context).textTheme.headline5,
+                              ),
+                              const SizedBox(width: 2),
+                              Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  const SizedBox(height: 2),
+                                  Text(
+                                    '%',
+                                    style:
+                                        Theme.of(context).textTheme.subtitle2,
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
                       ],
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 54.0,
+          child: widget.selectedItems > 0
+              ? AppBar(
+                  title: Text('${widget.selectedItems} selected'),
+                  centerTitle: false,
+                  leading: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        widget.unselect();
+                      });
+                    },
+                    icon: const Icon(Icons.close),
+                  ),
+                  actions: [
+                    IconButton(
+                      onPressed: () {
+                        widget.removeSelected(context);
+                      },
+                      icon: const Icon(Icons.delete),
                     ),
                   ],
-                ),
-              ),
-            ],
-          );
-        },
-      ),
+                )
+              : null,
+        ),
+      ],
     );
   }
 }
--- lib/widgets/pages/cabins_page.dart
+++ lib/widgets/pages/cabins_page.dart
@@ -8,29 +8,25 @@ import 'package:provider/provider.dart';
 class CabinsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 182),
-      child: SingleChildScrollView(
-        child: Consumer<CabinManager>(
-          builder: (context, cabinManager, child) {
-            return CabinsTable(
-              cabinRows: [
-                for (Cabin cabin in cabinManager.cabins)
-                  CabinTableRow(
-                    number: cabin.number,
-                    bookingsCount: cabin.bookings.length,
-                    recurringBookingsCount: cabin.recurringBookings.length,
-                    occupancyRate: cabin.evertimeOccupiedRatio(
-                      startTime: timeTableStartTime,
-                      endTime: timeTableEndTime,
-                      dates: cabinManager.allCabinsDatesWithBookings(),
-                    ),
-                  ),
-              ],
-            );
-          },
-        ),
-      ),
+    return Consumer<CabinManager>(
+      builder: (context, cabinManager, child) {
+        return CabinsTable(
+          cabinRows: [
+            for (Cabin cabin in cabinManager.cabins)
+              CabinTableRow(
+                id: cabin.id,
+                number: cabin.number,
+                bookingsCount: cabin.bookings.length,
+                recurringBookingsCount: cabin.recurringBookings.length,
+                occupancyRate: cabin.evertimeOccupiedRatio(
+                  startTime: timeTableStartTime,
+                  endTime: timeTableEndTime,
+                  dates: cabinManager.allCabinsDatesWithBookings(),
+                ),
+              ),
+          ],
+        );
+      },
     );
   }
 }
