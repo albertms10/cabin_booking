4e483dd0f2bbacd37f0c599892fbe54eed9f9d67
albert <albertmasa2@gmail.com>
2020-11-26 14:18:15 +0100

    refactor(data_dialog): extract widget

--- lib/widgets/booking/booking_dialog.dart
+++ lib/widgets/booking/booking_dialog.dart
@@ -2,6 +2,7 @@ import 'package:cabin_booking/l10n/app_localizations.dart';
 import 'package:cabin_booking/model/booking.dart';
 import 'package:cabin_booking/model/recurring_booking.dart';
 import 'package:cabin_booking/widgets/booking/booking_form.dart';
+import 'package:cabin_booking/widgets/layout/data_dialog.dart';
 import 'package:flutter/material.dart';
 
 class BookingDialog extends StatelessWidget {
@@ -11,36 +12,18 @@ class BookingDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SimpleDialog(
-      title: Row(children: [
-        IconButton(
-          icon: const Icon(Icons.arrow_back),
-          tooltip: MaterialLocalizations.of(context).backButtonTooltip,
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          splashRadius: 24.0,
-        ),
-        const SizedBox(width: 8.0),
-        Text(
-          booking.isDisabled
-              ? AppLocalizations.of(context).lockedRange
-              : booking is RecurringBooking ||
-                      booking.recurringBookingId != null
-                  ? AppLocalizations.of(context).recurringBooking
-                  : AppLocalizations.of(context).booking,
-        )
-      ]),
-      contentPadding:
-          const EdgeInsets.only(left: 24.0, right: 24.0, bottom: 24.0),
-      titlePadding:
-          const EdgeInsets.symmetric(horizontal: 12.0, vertical: 18.0),
-      children: [
-        SizedBox(
-          width: 250.0,
-          child: BookingForm(booking),
-        ),
-      ],
+    return DataDialog(
+      title: Text(
+        booking.isDisabled
+            ? AppLocalizations.of(context).lockedRange
+            : booking is RecurringBooking || booking.recurringBookingId != null
+                ? AppLocalizations.of(context).recurringBooking
+                : AppLocalizations.of(context).booking,
+      ),
+      content: SizedBox(
+        width: 250.0,
+        child: BookingForm(booking),
+      ),
     );
   }
 }
---
+++ lib/widgets/layout/data_dialog.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class DataDialog extends StatelessWidget {
+  final Widget title;
+  final Widget content;
+
+  DataDialog({this.title, this.content});
+
+  @override
+  Widget build(BuildContext context) {
+    return SimpleDialog(
+      title: Row(
+        children: [
+          IconButton(
+            icon: const Icon(Icons.arrow_back),
+            tooltip: MaterialLocalizations.of(context).backButtonTooltip,
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            splashRadius: 24.0,
+          ),
+          const SizedBox(width: 8.0),
+          title,
+        ],
+      ),
+      contentPadding:
+          const EdgeInsets.only(left: 24.0, right: 24.0, bottom: 24.0),
+      titlePadding:
+          const EdgeInsets.symmetric(horizontal: 12.0, vertical: 18.0),
+      children: [content],
+    );
+  }
+}
