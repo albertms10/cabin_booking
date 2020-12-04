052d0116f6460614667b1aa0b54bfe0b652e4af7
albert <albertmasa2@gmail.com>
2020-12-04 23:10:24 +0100

    fix(cabin_dropdown): remove unused import

--- lib/widgets/cabin/cabin_dropdown.dart
+++ lib/widgets/cabin/cabin_dropdown.dart
@@ -1,4 +1,3 @@
-import 'package:cabin_booking/model/cabin.dart';
 import 'package:cabin_booking/model/cabin_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
