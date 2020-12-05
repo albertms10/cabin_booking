e45fb088ae86896732c1c69f96a418597a05b2c4
albert <albertmasa2@gmail.com>
2020-12-05 18:51:28 +0100

    build(windows): change app name

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -30,7 +30,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(&run_loop, project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.CreateAndShow(L"cabin_booking", origin, size)) {
+  if (!window.CreateAndShow(L"Cabin Booking", origin, size))
+  {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
