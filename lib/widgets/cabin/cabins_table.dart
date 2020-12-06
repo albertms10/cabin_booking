c0c8462d0567c5c7dc1d6a774adcf3d9f75da4d7
albert <albertmasa2@gmail.com>
2020-12-06 23:42:09 +0100

    feat(centered_icon_message): add widget

--- lib/widgets/cabin/cabins_table.dart
+++ lib/widgets/cabin/cabins_table.dart
@@ -1,5 +1,6 @@
 import 'package:cabin_booking/model/cabin_manager.dart';
 import 'package:cabin_booking/widgets/cabin/cabin_icon.dart';
+import 'package:cabin_booking/widgets/layout/centered_icon_message.dart';
 import 'package:cabin_booking/widgets/layout/data_table_toolbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -160,178 +161,187 @@ class _CabinsTableState extends State<CabinsTable> {
   Widget build(BuildContext context) {
     final appLocalizations = AppLocalizations.of(context);
 
-    return Stack(
-      children: [
-        ListView(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(54.0),
-              child: DataTable(
-                dataRowHeight: 82.0,
-                showCheckboxColumn: true,
-                sortAscending: _sortAscending,
-                sortColumnIndex: _sortColumnIndex,
-                columns: [
-                  DataColumn(
-                    label: Padding(
-                      padding: const EdgeInsets.only(right: 8.0),
-                      child: Text(appLocalizations.cabin),
-                    ),
-                    onSort: onSortColumn,
-                  ),
-                  DataColumn(
-                    label: Padding(
-                      padding: const EdgeInsets.only(left: 8.0),
-                      child: Text(appLocalizations.bookings),
-                    ),
-                    numeric: true,
-                    onSort: onSortColumn,
-                  ),
-                  DataColumn(
-                    label: Padding(
-                      padding: const EdgeInsets.only(left: 8.0),
-                      child: Text(appLocalizations.recurringBookings),
-                    ),
-                    numeric: true,
-                    onSort: onSortColumn,
-                  ),
-                  DataColumn(
-                    label: Padding(
-                      padding: const EdgeInsets.only(left: 8.0),
-                      child: Text(appLocalizations.hours),
-                    ),
-                    numeric: true,
-                    onSort: onSortColumn,
-                  ),
-                  DataColumn(
-                    label: Padding(
-                      padding: const EdgeInsets.only(left: 8.0),
-                      child: Text(appLocalizations.occupancyRate),
-                    ),
-                    numeric: true,
-                    onSort: onSortColumn,
-                  ),
-                  DataColumn(
-                    label: Padding(
-                      padding: const EdgeInsets.only(left: 8.0),
-                      child: Text(
-                        appLocalizations.mostOccupiedTimeRange,
-                      ),
-                    ),
-                  ),
-                ],
-                rows: List<DataRow>.generate(
-                  widget.cabinRows.length,
-                  (index) {
-                    final cabinRow = widget.cabinRows[index];
-
-                    return DataRow(
-                      selected: widget.cabinRows[index].selected,
-                      onSelectChanged: (selected) {
-                        setState(
-                          () => widget.cabinRows[index].selected = selected,
-                        );
-                      },
-                      cells: [
-                        DataCell(
-                          CabinIcon(number: cabinRow.number),
-                        ),
-                        DataCell(
-                          Text(
-                            '${cabinRow.bookingsCount}',
-                            style: Theme.of(context).textTheme.headline5,
+    return widget.cabinRows.isEmpty
+        ? CenteredIconMessage(
+            icon: Icons.sensor_door,
+            message: appLocalizations.noCabinsMessage,
+          )
+        : Stack(
+            children: [
+              ListView(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(54.0),
+                    child: DataTable(
+                      dataRowHeight: 82.0,
+                      showCheckboxColumn: true,
+                      sortAscending: _sortAscending,
+                      sortColumnIndex: _sortColumnIndex,
+                      columns: [
+                        DataColumn(
+                          label: Padding(
+                            padding: const EdgeInsets.only(right: 8.0),
+                            child: Text(appLocalizations.cabin),
                           ),
+                          onSort: onSortColumn,
                         ),
-                        DataCell(
-                          Text(
-                            '${cabinRow.recurringBookingsCount}',
-                            style: Theme.of(context).textTheme.headline5,
-                          ),
-                        ),
-                        DataCell(
-                          Text(
-                            '${cabinRow.accumulatedDuration.inHours}',
-                            style: Theme.of(context).textTheme.headline5,
-                          ),
-                        ),
-                        DataCell(
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Text(
-                                '${(cabinRow.occupancyRate * 100).round()}',
-                                style: Theme.of(context).textTheme.headline5,
-                              ),
-                              const SizedBox(width: 2),
-                              Column(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  const SizedBox(height: 2),
-                                  Text(
-                                    '%',
-                                    style:
-                                        Theme.of(context).textTheme.subtitle2,
-                                  )
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                        DataCell(
-                          Padding(
+                        DataColumn(
+                          label: Padding(
                             padding: const EdgeInsets.only(left: 8.0),
-                            child: Wrap(
-                              spacing: 8.0,
-                              runSpacing: 6.0,
-                              children: [
-                                for (final timeRange
-                                    in cabinRow.mostOccupiedTimeRanges)
-                                  Chip(
-                                    label: Text(
-                                      '${timeRange.first.format(context)}–${timeRange.last.format(context)}',
-                                    ),
-                                  )
-                              ],
+                            child: Text(appLocalizations.bookings),
+                          ),
+                          numeric: true,
+                          onSort: onSortColumn,
+                        ),
+                        DataColumn(
+                          label: Padding(
+                            padding: const EdgeInsets.only(left: 8.0),
+                            child: Text(appLocalizations.recurringBookings),
+                          ),
+                          numeric: true,
+                          onSort: onSortColumn,
+                        ),
+                        DataColumn(
+                          label: Padding(
+                            padding: const EdgeInsets.only(left: 8.0),
+                            child: Text(appLocalizations.hours),
+                          ),
+                          numeric: true,
+                          onSort: onSortColumn,
+                        ),
+                        DataColumn(
+                          label: Padding(
+                            padding: const EdgeInsets.only(left: 8.0),
+                            child: Text(appLocalizations.occupancyRate),
+                          ),
+                          numeric: true,
+                          onSort: onSortColumn,
+                        ),
+                        DataColumn(
+                          label: Padding(
+                            padding: const EdgeInsets.only(left: 8.0),
+                            child: Text(
+                              appLocalizations.mostOccupiedTimeRange,
                             ),
                           ),
                         ),
                       ],
-                    );
-                  },
-                ),
+                      rows: List<DataRow>.generate(
+                        widget.cabinRows.length,
+                        (index) {
+                          final cabinRow = widget.cabinRows[index];
+
+                          return DataRow(
+                            selected: widget.cabinRows[index].selected,
+                            onSelectChanged: (selected) {
+                              setState(
+                                () =>
+                                    widget.cabinRows[index].selected = selected,
+                              );
+                            },
+                            cells: [
+                              DataCell(
+                                CabinIcon(number: cabinRow.number),
+                              ),
+                              DataCell(
+                                Text(
+                                  '${cabinRow.bookingsCount}',
+                                  style: Theme.of(context).textTheme.headline5,
+                                ),
+                              ),
+                              DataCell(
+                                Text(
+                                  '${cabinRow.recurringBookingsCount}',
+                                  style: Theme.of(context).textTheme.headline5,
+                                ),
+                              ),
+                              DataCell(
+                                Text(
+                                  '${cabinRow.accumulatedDuration.inHours}',
+                                  style: Theme.of(context).textTheme.headline5,
+                                ),
+                              ),
+                              DataCell(
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Text(
+                                      '${(cabinRow.occupancyRate * 100).round()}',
+                                      style:
+                                          Theme.of(context).textTheme.headline5,
+                                    ),
+                                    const SizedBox(width: 2),
+                                    Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        const SizedBox(height: 2),
+                                        Text(
+                                          '%',
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .subtitle2,
+                                        )
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              DataCell(
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 8.0),
+                                  child: Wrap(
+                                    spacing: 8.0,
+                                    runSpacing: 6.0,
+                                    children: [
+                                      for (final timeRange
+                                          in cabinRow.mostOccupiedTimeRanges)
+                                        Chip(
+                                          label: Text(
+                                            '${timeRange.first.format(context)}–${timeRange.last.format(context)}',
+                                          ),
+                                        )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
-          ],
-        ),
-        DataTableToolbar(
-          shown: widget.selectedItems > 0,
-          selectedItems: widget.selectedItems,
-          onPressedLeading: () {
-            setState(() {
-              widget.unselect();
-            });
-          },
-          actions: [
-            IconButton(
-              onPressed: widget.selectedAreBooked
-                  ? () {
-                      widget.emptySelected(context);
-                    }
-                  : null,
-              icon: const Icon(Icons.delete_outline),
-              tooltip: appLocalizations.empty,
-            ),
-            IconButton(
-              onPressed: () {
-                widget.removeSelected(context);
-              },
-              icon: const Icon(Icons.delete),
-              tooltip: MaterialLocalizations.of(context).deleteButtonTooltip,
-            ),
-          ],
-        ),
-      ],
-    );
+              DataTableToolbar(
+                shown: widget.selectedItems > 0,
+                selectedItems: widget.selectedItems,
+                onPressedLeading: () {
+                  setState(() {
+                    widget.unselect();
+                  });
+                },
+                actions: [
+                  IconButton(
+                    onPressed: widget.selectedAreBooked
+                        ? () {
+                            widget.emptySelected(context);
+                          }
+                        : null,
+                    icon: const Icon(Icons.delete_outline),
+                    tooltip: appLocalizations.empty,
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      widget.removeSelected(context);
+                    },
+                    icon: const Icon(Icons.delete),
+                    tooltip:
+                        MaterialLocalizations.of(context).deleteButtonTooltip,
+                  ),
+                ],
+              ),
+            ],
+          );
   }
 }
---
+++ lib/widgets/layout/centered_icon_message.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class CenteredIconMessage extends StatelessWidget {
+  final IconData icon;
+  final String message;
+
+  const CenteredIconMessage({
+    Key key,
+    this.icon,
+    this.message,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Icon(
+            icon,
+            size: 72.0,
+            color: Colors.grey,
+          ),
+          const SizedBox(height: 24.0),
+          Text(
+            message,
+            style: Theme.of(context)
+                .textTheme
+                .headline5
+                .copyWith(color: Colors.grey[600]),
+          ),
+          const SizedBox(height: 82.0),
+        ],
+      ),
+    );
+  }
+}
