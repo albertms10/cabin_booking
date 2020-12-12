a1099aaa01ae89e9ec937265472dcadffaf3ad5f
albertms10 <albertmasa2@gmail.com>
2020-12-12 09:51:37 +0100

    feat(constants): extend timeTableStartTime value

--- lib/constants.dart
+++ lib/constants.dart
@@ -6,5 +6,5 @@ const double columnWidth = 182.0;
 const Duration minSlotDuration = Duration(minutes: 20);
 const Duration maxSlotDuration = Duration(minutes: 60);
 
-const TimeOfDay timeTableStartTime = TimeOfDay(hour: 15, minute: 0);
+const TimeOfDay timeTableStartTime = TimeOfDay(hour: 9, minute: 0);
 const TimeOfDay timeTableEndTime = TimeOfDay(hour: 22, minute: 0);
