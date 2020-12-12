99f6099350132e4305396179e37834c7923ab4f8
albertms10 <albertmasa2@gmail.com>
2020-12-12 15:21:20 +0100

    feat(cabin_form): add cabin components fields

--- lib/widgets/cabin/cabin_form.dart
+++ lib/widgets/cabin/cabin_form.dart
@@ -2,6 +2,8 @@ import 'package:cabin_booking/model/cabin.dart';
 import 'package:cabin_booking/widgets/cabin/cabin_icon.dart';
 import 'package:cabin_booking/widgets/layout/submit_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class CabinForm extends StatefulWidget {
   final Cabin cabin;
@@ -32,39 +34,76 @@ class _CabinFormState extends State<CabinForm> {
     }
   }
 
+  String _validator(value) {
+    if (value.isEmpty) {
+      return AppLocalizations.of(context).enterValue;
+    }
+
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
+    final appLocalizations = AppLocalizations.of(context);
+
     return Form(
       key: _formKey,
       child: Column(
         children: [
           CabinIcon(number: _cabin.number),
-          DataTable(
-            columns: const [
-              DataColumn(
-                label: Text('Element'),
-              ),
-              DataColumn(
-                label: Text('Quantity'),
-                numeric: true,
-              ),
-            ],
-            rows: [
-              DataRow(
-                cells: [
-                  DataCell(
-                    const Text('Pianos'),
-                    onTap: () {},
-                    showEditIcon: true,
-                  ),
-                  DataCell(
-                    const Text('1'),
-                    onTap: () {},
-                    showEditIcon: true,
-                  ),
+          const SizedBox(height: 32.0),
+          ListTile(
+            title: Text(appLocalizations.lecterns),
+            trailing: SizedBox(
+              width: 80.0,
+              child: TextFormField(
+                initialValue: '${_cabin.components.lecterns}',
+                keyboardType: TextInputType.number,
+                inputFormatters: [
+                  FilteringTextInputFormatter.digitsOnly,
                 ],
+                validator: _validator,
+                onSaved: (lecterns) {
+                  _cabin.components.lecterns = int.tryParse(lecterns);
+                },
               ),
-            ],
+            ),
+          ),
+          const SizedBox(height: 16.0),
+          ListTile(
+            title: Text(appLocalizations.chairs),
+            trailing: SizedBox(
+              width: 80.0,
+              child: TextFormField(
+                initialValue: '${_cabin.components.chairs}',
+                keyboardType: TextInputType.number,
+                inputFormatters: [
+                  FilteringTextInputFormatter.digitsOnly,
+                ],
+                validator: _validator,
+                onSaved: (chairs) {
+                  _cabin.components.chairs = int.tryParse(chairs);
+                },
+              ),
+            ),
+          ),
+          const SizedBox(height: 16.0),
+          ListTile(
+            title: Text(appLocalizations.tables),
+            trailing: SizedBox(
+              width: 80.0,
+              child: TextFormField(
+                initialValue: '${_cabin.components.tables}',
+                keyboardType: TextInputType.number,
+                inputFormatters: [
+                  FilteringTextInputFormatter.digitsOnly,
+                ],
+                validator: _validator,
+                onSaved: (tables) {
+                  _cabin.components.tables = int.tryParse(tables);
+                },
+              ),
+            ),
           ),
           const SizedBox(height: 32.0),
           SubmitButton(
