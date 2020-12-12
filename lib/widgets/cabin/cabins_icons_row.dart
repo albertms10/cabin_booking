d2c4f3e923ae945261510e0d6778640d12ffa675
albertms10 <albertmasa2@gmail.com>
2020-12-12 12:37:40 +0100

    feat(cabins_icons_row): add noCabins text

--- lib/widgets/cabin/cabins_icons_row.dart
+++ lib/widgets/cabin/cabins_icons_row.dart
@@ -3,6 +3,7 @@ import 'package:cabin_booking/model/cabin_manager.dart';
 import 'package:cabin_booking/model/day_handler.dart';
 import 'package:cabin_booking/widgets/cabin/cabin_icon.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
 
 class CabinsIconsRow extends StatelessWidget {
@@ -18,18 +19,30 @@ class CabinsIconsRow extends StatelessWidget {
           return Row(
             children: [
               child,
-              for (final cabin in cabinManager.cabins)
-                SizedBox(
-                  width: columnWidth,
-                  child: CabinIcon(
-                    number: cabin.number,
-                    progress: cabin.occupiedRatioOn(
-                      dayHandler.dateTime,
-                      startTime: timeTableStartTime,
-                      endTime: timeTableEndTime,
+              if (cabinManager.cabins.isEmpty)
+                Expanded(
+                  child: Text(
+                    AppLocalizations.of(context).noCabins,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline5
+                        .copyWith(color: Colors.grey[600]),
+                  ),
+                )
+              else
+                for (final cabin in cabinManager.cabins)
+                  SizedBox(
+                    width: columnWidth,
+                    child: CabinIcon(
+                      number: cabin.number,
+                      progress: cabin.occupiedRatioOn(
+                        dayHandler.dateTime,
+                        startTime: timeTableStartTime,
+                        endTime: timeTableEndTime,
+                      ),
                     ),
                   ),
-                ),
             ],
           );
         },
