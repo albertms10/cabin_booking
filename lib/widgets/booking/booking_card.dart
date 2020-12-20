6072b1c18568df5e5b1befc0068a7646244bd8f6
albertms10 <albertmasa2@gmail.com>
2020-12-20 18:19:43 +0100

    feat(booking_card): improve display style

--- lib/widgets/booking/booking_card.dart
+++ lib/widgets/booking/booking_card.dart
@@ -60,49 +60,97 @@ class BookingCard extends StatelessWidget {
 
         return Card(
           margin: const EdgeInsets.all(8.0),
-          shadowColor: isBeforeNow ? Colors.black38 : Colors.black,
+          shadowColor: isBeforeNow ? Colors.black38 : Colors.black87,
+          elevation: 0.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: const BorderRadius.all(Radius.circular(10.0)),
+            side: BorderSide(
+              color: (isRecurring ? Colors.blue[200] : Colors.grey[300])
+                  .withAlpha(isBeforeNow ? 100 : 255),
+              width: 1.5,
+            ),
+          ),
           color: Colors.transparent,
           child: Container(
             height: height,
             padding: const EdgeInsets.all(8.0),
             decoration: BoxDecoration(
-              color: isRecurring
-                  ? Colors.blue[50]
-                  : isBeforeNow
-                      ? const Color.fromARGB(150, 255, 255, 255)
-                      : Colors.white,
-              borderRadius: const BorderRadius.all(Radius.circular(4.0)),
+              color: (isRecurring ? Colors.blue[50] : Colors.white)
+                  .withAlpha(isBeforeNow ? 100 : 255),
+              borderRadius: const BorderRadius.all(Radius.circular(10.0)),
             ),
-            child: Row(
+            child: BookingCardInfo(
+              cabin: cabin,
+              booking: booking,
+              isRecurring: isRecurring,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class BookingCardInfo extends StatelessWidget {
+  final Cabin cabin;
+  final Booking booking;
+  final bool isRecurring;
+
+  const BookingCardInfo({
+    Key key,
+    @required this.cabin,
+    @required this.booking,
+    this.isRecurring = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Stack(
+          alignment: Alignment.topRight,
+          children: [
+            Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(booking.description),
+                Text(
+                  booking.description,
+                  style: TextStyle(
+                      fontSize: constraints.maxHeight > 20
+                          ? 14.0
+                          : constraints.maxHeight * 0.5),
+                ),
+                Row(
+                  children: [
+                    if (constraints.maxHeight > 30)
                       Text(
                         booking.timeRange,
-                        style: const TextStyle(color: Colors.black38),
-                      ),
-                      if (isRecurring)
-                        Padding(
-                          padding: const EdgeInsets.only(top: 4.0),
-                          child: Text(
-                            '${booking.recurringNumber}/${booking.recurringTotalTimes}',
-                            style: Theme.of(context).textTheme.caption,
-                          ),
+                        style: TextStyle(
+                          color: Colors.black38,
+                          fontSize: constraints.maxHeight > 40
+                              ? 14.0
+                              : constraints.maxHeight * 0.4,
                         ),
-                    ],
-                  ),
-                ),
-                BookingPopupMenu(
-                  cabin: cabin,
-                  booking: booking,
+                      ),
+                    if (isRecurring && constraints.maxHeight > 30)
+                      Text(
+                        ' · ${booking.recurringNumber}/${booking.recurringTotalTimes}',
+                        style: Theme.of(context).textTheme.caption.copyWith(
+                              color: Colors.black38,
+                              fontSize: constraints.maxHeight > 40
+                                  ? 14.0
+                                  : constraints.maxHeight * 0.4,
+                            ),
+                      ),
+                  ],
                 ),
               ],
             ),
-          ),
+            BookingPopupMenu(
+              cabin: cabin,
+              booking: booking,
+            ),
+          ],
         );
       },
     );
