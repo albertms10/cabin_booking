395f4eec428db797d36406a710542d6007fddbec
albertms10 <albertmasa2@gmail.com>
2020-12-26 12:10:55 +0100

    refactor(booking_manager): use Map containsKey method

--- lib/model/booking_manager.dart
+++ lib/model/booking_manager.dart
@@ -168,7 +168,7 @@ class BookingManager with ChangeNotifier {
 
     for (final booking in allBookings) {
       for (final bookingTimeRange in booking.hoursSpan.entries) {
-        if (timeRanges[bookingTimeRange.key] != null) {
+        if (timeRanges.containsKey(bookingTimeRange.key)) {
           timeRanges[bookingTimeRange.key] += bookingTimeRange.value;
         } else {
           timeRanges[bookingTimeRange.key] = bookingTimeRange.value;
