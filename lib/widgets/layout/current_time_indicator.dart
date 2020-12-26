5551b008eee52bfe896ca63f0b2a093a685b366b
albertms10 <albertmasa2@gmail.com>
2020-12-26 01:20:21 +0100

    refactor(horizontal_indicator): extract Widget

--- lib/widgets/layout/current_time_indicator.dart
+++ lib/widgets/layout/current_time_indicator.dart
@@ -1,6 +1,7 @@
 import 'package:cabin_booking/constants.dart';
 import 'package:cabin_booking/model/day_handler.dart';
 import 'package:cabin_booking/utils/date.dart';
+import 'package:cabin_booking/widgets/layout/horizontal_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:timer_builder/timer_builder.dart';
@@ -34,39 +35,18 @@ class CurrentTimeIndicator extends StatelessWidget {
           return const SizedBox();
         }
 
-        return Column(
-          children: [
-            SizedBox(
-              height: durationFromStart.inMicroseconds /
-                  Duration.microsecondsPerMinute *
-                  bookingHeightRatio,
-              child: Container(
-                alignment: Alignment.bottomLeft,
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  TimeOfDay.fromDateTime(DateTime.now()).format(context),
-                  style: TextStyle(
-                    color: Colors.red[400],
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
+        return HorizontalIndicator(
+          verticalOffset: durationFromStart.inMicroseconds /
+              Duration.microsecondsPerMinute *
+              bookingHeightRatio,
+          label: Text(
+            TimeOfDay.fromDateTime(DateTime.now()).format(context),
+            style: TextStyle(
+              color: Colors.red[400],
+              fontWeight: FontWeight.bold,
             ),
-            Container(
-              width: double.infinity,
-              height: 2.0,
-              decoration: BoxDecoration(
-                color: Colors.red[400],
-                boxShadow: const [
-                  BoxShadow(
-                    color: Colors.black12,
-                    blurRadius: 3.0,
-                    offset: Offset(3.0, 4.0),
-                  ),
-                ],
-              ),
-            ),
-          ],
+          ),
+          indicatorColor: Colors.red[400],
         );
       },
     );
---
+++ lib/widgets/layout/horizontal_indicator.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class HorizontalIndicator extends StatelessWidget {
+  final double verticalOffset;
+  final Widget label;
+  final Color indicatorColor;
+
+  const HorizontalIndicator({
+    Key key,
+    this.verticalOffset,
+    this.label,
+    this.indicatorColor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: verticalOffset,
+          child: Container(
+            alignment: Alignment.bottomLeft,
+            padding: const EdgeInsets.all(8.0),
+            child: label,
+          ),
+        ),
+        Container(
+          width: double.infinity,
+          height: 2.0,
+          decoration: BoxDecoration(
+            color: indicatorColor,
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 3.0,
+                offset: Offset(3.0, 4.0),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
