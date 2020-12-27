dc964908af5618d0131de1096c6fc76864fb5580
albertms10 <albertmasa2@gmail.com>
2020-12-27 15:17:32 +0100

    fix(map_number): relax double parameters to num

--- lib/utils/map_number.dart
+++ lib/utils/map_number.dart
@@ -2,10 +2,10 @@
 
 /// Re-maps a number from one range to another (default from 0 to 1).
 double mapNumber(
-  double value, {
-  double inMin = 0.0,
-  double inMax = 1.0,
-  double outMin = 0.0,
-  double outMax = 1.0,
+  num value, {
+  num inMin = 0.0,
+  num inMax = 1.0,
+  num outMin = 0.0,
+  num outMax = 1.0,
 }) =>
     ((value - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
