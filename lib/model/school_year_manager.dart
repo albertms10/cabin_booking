036022aa346b24dce43fdcde85aebf9e8c16aadd
albertms10 <albertmasa2@gmail.com>
2021-01-01 01:45:42 +0100

    feat(school_year_manager): extend WritableManager and implement methods

--- lib/model/school_year_manager.dart
+++ lib/model/school_year_manager.dart
@@ -1,12 +1,30 @@
-import 'package:cabin_booking/model/school_year.dart';
+import 'dart:convert' show json;
 
-class SchoolYearManager {
+import 'package:cabin_booking/model/file_manager.dart';
+import 'package:cabin_booking/model/school_year.dart';
+import 'package:cabin_booking/model/writable_manager.dart';
+import 'package:flutter/foundation.dart';
+
+Set<SchoolYear> _parseSchoolYears(String jsonString) => json
+    .decode(jsonString)
+    .map<SchoolYear>((json) => SchoolYear.from(json))
+    .toSet();
+
+class SchoolYearManager extends WritableManager<Set<SchoolYear>>
+    with ChangeNotifier, FileManager {
   Set<SchoolYear> schoolYears;
   int schoolYearIndex;
 
-  SchoolYearManager(this.schoolYears) {
+  SchoolYearManager({
+    this.schoolYears,
+    String fileName = 'school_year_manager',
+  }) : super(fileName) {
+    schoolYears ??= <SchoolYear>{};
     schoolYearIndex = _currentSchoolYearIndex;
   }
+
+  List<Map<String, dynamic>> schoolYearsToMapList() =>
+      schoolYears.map((schoolYear) => schoolYear.toMap()).toList();
 
   int get _currentSchoolYearIndex => _schoolYearIndexFrom(DateTime.now());
 
@@ -37,4 +55,38 @@ class SchoolYearManager {
       schoolYearIndex = schoolYearIndex < schoolYears.length - 1
           ? schoolYearIndex + 1
           : schoolYears.length - 1;
+
+  @override
+  Future<Set<SchoolYear>> readFromFile() async {
+    try {
+      final file = await localFile(fileName);
+      final content = await file.readAsString();
+
+      final schoolYears = await compute(_parseSchoolYears, content);
+
+      return schoolYears;
+    } catch (e) {
+      return <SchoolYear>{};
+    }
+  }
+
+  @override
+  Future<int> loadFromFile() async {
+    schoolYears = await readFromFile();
+
+    notifyListeners();
+
+    return schoolYears.length;
+  }
+
+  @override
+  Future<bool> writeToFile() async {
+    final file = await localFile(fileName);
+
+    await file.writeAsString(
+      json.encode(schoolYearsToMapList()),
+    );
+
+    return true;
+  }
 }
