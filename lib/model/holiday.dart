61ac60233eb0ca2df90b829d96317333e5c27ec5
albertms10 <albertmasa2@gmail.com>
2021-01-01 01:43:00 +0100

    feat(holiday): add class and SchoolYear set

---
+++ lib/model/holiday.dart
@@ -0,0 +1,5 @@
+import 'package:cabin_booking/model/date_range.dart';
+
+class Holiday extends DateRange {
+  Holiday.from(Map<String, dynamic> other) : super.from(other);
+}
--- lib/model/school_year.dart
+++ lib/model/school_year.dart
@@ -1,17 +1,25 @@
 import 'package:cabin_booking/model/date_range.dart';
+import 'package:cabin_booking/model/holiday.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class SchoolYear extends DateRange {
+  Set<Holiday> holidays;
+
   SchoolYear({
     String id,
     @required DateTime startDate,
     @required DateTime endDate,
+    this.holidays,
   }) : super(
           id: id,
           startDate: startDate,
           endDate: endDate,
-        );
+        ) {
+    holidays ??= <Holiday>{};
+  }
+
+  SchoolYear.from(Map<String, dynamic> other) : super.from(other);
 
   @override
   String toString() =>
