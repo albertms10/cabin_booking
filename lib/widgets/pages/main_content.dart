9aa218ca82469f90c531a5c5dadf758fd5952a6c
albertms10 <albertmasa2@gmail.com>
2021-01-01 01:48:01 +0100

    feat(main_content): load schoolYearManager
    * rewrite future loading with FutureProvider

--- lib/widgets/pages/main_content.dart
+++ lib/widgets/pages/main_content.dart
@@ -1,4 +1,7 @@
 import 'package:cabin_booking/model/cabin_manager.dart';
+import 'package:cabin_booking/model/day_handler.dart';
+import 'package:cabin_booking/model/school_year_manager.dart';
+import 'package:cabin_booking/model/writable_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
@@ -15,55 +18,94 @@ class MainContent extends StatefulWidget {
 
 class _MainContentState extends State<MainContent> {
   CabinManager _cabinManager;
-  Future<int> _cabinsFuture;
+  SchoolYearManager _schoolYearManager;
 
-  void _writeAndShowSnackBar() async {
-    await _cabinManager.writeCabinsToFile();
+  void _writeAndShowSnackBar(WritableManager manager) async {
+    await manager.writeToFile();
 
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
-        content: Text(AppLocalizations.of(context).changesSaved),
+        content: Row(
+          children: [
+            Expanded(
+              child: Text(AppLocalizations.of(context).changesSaved),
+            ),
+            Chip(label: Text('${manager.runtimeType}')),
+          ],
+        ),
         duration: const Duration(seconds: 1),
       ),
     );
   }
 
+  void _writeCabinsAndShowSnackBar() => _writeAndShowSnackBar(_cabinManager);
+
+  void _writeSchoolYearsAndShowSnackBar() =>
+      _writeAndShowSnackBar(_schoolYearManager);
+
   @override
   void initState() {
     super.initState();
 
-    _cabinManager = Provider.of<CabinManager>(context, listen: false);
+    _cabinManager = Provider.of<CabinManager>(context, listen: false)
+      ..addListener(_writeCabinsAndShowSnackBar);
 
-    _cabinManager.addListener(_writeAndShowSnackBar);
-
-    _cabinsFuture = _cabinManager.loadCabinsFromFile();
+    _schoolYearManager = Provider.of<DayHandler>(context, listen: false)
+        .schoolYearManager
+      ..addListener(_writeSchoolYearsAndShowSnackBar);
   }
 
   @override
   void dispose() {
-    _cabinManager.removeListener(_writeAndShowSnackBar);
+    _cabinManager.removeListener(_writeCabinsAndShowSnackBar);
+    _schoolYearManager.removeListener(_writeSchoolYearsAndShowSnackBar);
 
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<int>(
-      future: _cabinsFuture,
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return Center(
-            child: Text(
-              AppLocalizations.of(context).dataCouldNotBeLoaded,
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          );
-        } else if (snapshot.hasData) {
+    return FutureProvider<List<int>>(
+      create: (context) => Future.wait([
+        _cabinManager.loadFromFile(),
+        _schoolYearManager.loadFromFile(),
+      ]),
+      initialData: const [],
+      child: Consumer<List<int>>(
+        builder: (context, items, child) {
+          if (items == null) {
+            return Center(
+              child: Text(
+                AppLocalizations.of(context).dataCouldNotBeLoaded,
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            );
+          } else if (items.isEmpty) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
           return widget.pages[widget.railIndex];
-        } else {
-          return const Center(child: CircularProgressIndicator());
-        }
-      },
+        },
+      ),
     );
   }
 }
+
+/*
+return FutureProvider<List<int>>(
+      create: (context) => Future.wait([
+        _cabinManager.loadFromFile(),
+        _schoolYearManager.loadFromFile(),
+      ]),
+      initialData: const Center(child: CircularProgressIndicator()),
+      child: widget.pages[widget.railIndex],
+      catchError: (context, error) {
+        return Center(
+          child: Text(
+            AppLocalizations.of(context).dataCouldNotBeLoaded,
+            style: Theme.of(context).textTheme.headline4,
+          ),
+        );
+      },
+    );
+*/
