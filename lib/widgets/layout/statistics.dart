843a28a708a5343490e611a76a13e786f544108e
albertms10 <albertmasa2@gmail.com>
2021-01-03 19:55:18 +0100

    feat(statistics): add InkWell onTap

--- lib/widgets/layout/statistics.dart
+++ lib/widgets/layout/statistics.dart
@@ -4,58 +4,59 @@ class Statistics extends StatelessWidget {
   final String title;
   final IconData icon;
   final List<StatisticItem> items;
+  final void Function() onTap;
 
   const Statistics({
     Key key,
     this.title,
     this.icon,
     this.items,
+    this.onTap,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            if (icon != null || title != null)
-              Padding(
-                padding: const EdgeInsets.only(bottom: 16.0),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  textBaseline: TextBaseline.ideographic,
-                  children: [
-                    if (icon != null)
-                      Padding(
-                        padding: const EdgeInsets.only(right: 8.0),
-                        child: Icon(
-                          icon,
-                          size: 18.0,
-                          color: Theme.of(context).hintColor,
+      child: InkWell(
+        onTap: onTap,
+        borderRadius: const BorderRadius.all(Radius.circular(4.0)),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              if (icon != null || title != null)
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 16.0),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    textBaseline: TextBaseline.ideographic,
+                    children: [
+                      if (icon != null)
+                        Padding(
+                          padding: const EdgeInsets.only(right: 8.0),
+                          child: Icon(
+                            icon,
+                            size: 18.0,
+                            color: Theme.of(context).hintColor,
+                          ),
                         ),
-                      ),
-                    if (title != null)
-                      Text(
-                        title,
-                        style: Theme.of(context).textTheme.subtitle1,
-                      ),
-                  ],
+                      if (title != null)
+                        Text(
+                          title,
+                          style: Theme.of(context).textTheme.subtitle1,
+                        ),
+                    ],
+                  ),
                 ),
+              Wrap(
+                spacing: 24.0,
+                runSpacing: 24.0,
+                children: items,
               ),
-            Row(
-              children: [
-                for (var i = 0; i < items.length; i++)
-                  Container(
-                    padding: (i == items.length - 1)
-                        ? null
-                        : const EdgeInsets.only(right: 32.0),
-                    child: items[i],
-                  )
-              ],
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
