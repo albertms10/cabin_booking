13e3fd9ce383eb4d34715b8c531a9d390b4ffbd7
albertms10 <albertmasa2@gmail.com>
2021-01-06 23:27:25 +0100

    feat(school_years_table): add count stats

--- lib/widgets/school_year/school_years_table.dart
+++ lib/widgets/school_year/school_years_table.dart
@@ -1,16 +1,19 @@
+import 'package:cabin_booking/constants.dart';
+import 'package:cabin_booking/model/cabin_manager.dart';
 import 'package:cabin_booking/model/day_handler.dart';
 import 'package:cabin_booking/model/school_year.dart';
+import 'package:cabin_booking/utils/compactize_range.dart';
 import 'package:cabin_booking/widgets/item/items_table.dart';
 import 'package:cabin_booking/widgets/school_year/school_year_dialog.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:provider/provider.dart';
 
 class SchoolYearsTable extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Consumer<DayHandler>(
-      builder: (context, dayHandler, child) {
+    return Consumer2<DayHandler, CabinManager>(
+      builder: (context, dayHandler, cabinManager, child) {
         final appLocalizations = AppLocalizations.of(context);
 
         return ItemsTable(
@@ -34,7 +37,26 @@ class SchoolYearsTable extends StatelessWidget {
           },
           rows: [
             for (final schoolYear in dayHandler.schoolYearManager.schoolYears)
-              ItemsTableRow<SchoolYear>(item: schoolYear),
+              ItemsTableRow<SchoolYear>(
+                item: schoolYear,
+                bookingsCount: cabinManager.bookingsCountBetween(schoolYear),
+                recurringBookingsCount:
+                    cabinManager.recurringBookingsCountBetween(schoolYear),
+                accumulatedDuration:
+                    cabinManager.totalOccupiedDuration(dateRange: schoolYear),
+                occupancyPercent: cabinManager.occupancyPercent(
+                  startTime: kTimeTableStartTime,
+                  endTime: kTimeTableEndTime,
+                  dates: cabinManager.allCabinsDatesWithBookings(schoolYear),
+                ),
+                mostOccupiedTimeRanges: compactizeRange<TimeOfDay>(
+                  cabinManager.mostOccupiedTimeRange(schoolYear),
+                  nextValue: (timeOfDay) => timeOfDay.replacing(
+                    hour: (timeOfDay.hour + 1) % TimeOfDay.hoursPerDay,
+                  ),
+                  inclusive: true,
+                ),
+              ),
           ],
         );
       },
