99ae9881029ce7e0c4e70484ac723437596aeb11
albertms10 <albertmasa2@gmail.com>
2021-01-16 20:52:47 +0100

    feat(detail_line_chart): add Widget

---
+++ lib/widgets/layout/detail_line_chart.dart
@@ -0,0 +1,54 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+
+class DetailLineChart extends StatelessWidget {
+  final List<FlSpot> spots;
+  final double minX;
+  final double maxX;
+  final double minY;
+  final double maxY;
+
+  const DetailLineChart({
+    @required this.spots,
+    this.minX,
+    this.maxX,
+    this.minY = 0.0,
+    this.maxY,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    final colors = [
+      theme.colorScheme.primary,
+    ];
+
+    return LineChart(
+      LineChartData(
+        minX: minX,
+        maxX: maxX,
+        minY: minY,
+        maxY: maxY,
+        gridData: FlGridData(show: false),
+        titlesData: FlTitlesData(show: false),
+        borderData: FlBorderData(show: false),
+        lineBarsData: [
+          LineChartBarData(
+            spots: spots,
+            isCurved: true,
+            preventCurveOverShooting: true,
+            colors: colors,
+            barWidth: 2.0,
+            isStrokeCapRound: true,
+            dotData: FlDotData(show: false),
+            belowBarData: BarAreaData(
+              show: true,
+              colors: colors.map((color) => color.withOpacity(0.3)).toList(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
