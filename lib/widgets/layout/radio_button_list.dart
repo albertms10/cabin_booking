bfb3e4c20016cb75f1a9c75deebf4f106f24e4ae
albertms10 <albertmasa2@gmail.com>
2021-01-20 16:42:08 +0100

    feat(radio_button_list): add Widget

---
+++ lib/widgets/layout/radio_button_list.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class RadioButtonList extends StatefulWidget {
+  final int itemCount;
+  final IndexedWidgetBuilder itemBuilder;
+  final int initialIndex;
+  final void Function(int) onChanged;
+  final bool reverse;
+
+  const RadioButtonList({
+    @required this.itemCount,
+    @required this.itemBuilder,
+    this.initialIndex,
+    this.onChanged,
+    this.reverse = false,
+  });
+
+  int get _initialLoopIndex => reverse ? itemCount - 1 : 0;
+
+  int get _lastLoopIndex => reverse ? 0 : itemCount - 1;
+
+  bool _loopCondition(int index) =>
+      reverse ? (index >= 0) : (index < itemCount);
+
+  @override
+  _RadioButtonListState createState() => _RadioButtonListState();
+}
+
+class _RadioButtonListState extends State<RadioButtonList> {
+  int _selectedIndex;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _selectedIndex = widget.initialIndex ?? 0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        for (var i = widget._initialLoopIndex;
+            widget._loopCondition(i);
+            widget.reverse ? i-- : i++)
+          Padding(
+            padding:
+                EdgeInsets.only(bottom: i == widget._lastLoopIndex ? 0.0 : 4.0),
+            child: FlatButton(
+              color: _selectedIndex == i ? Colors.blue : null,
+              textTheme: ButtonTextTheme.primary,
+              textColor:
+                  _selectedIndex == i ? null : Theme.of(context).hintColor,
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
+              onPressed: () {
+                if (i != _selectedIndex) {
+                  setState(() => _selectedIndex = i);
+
+                  widget.onChanged?.call(i);
+                }
+              },
+              child: widget.itemBuilder(context, i),
+            ),
+          ),
+      ],
+    );
+  }
+}
