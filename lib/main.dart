2d4ce04d5fda110ad4f801577e57a58e2d58ec45
Albert Mañosa <albertmasa2@gmail.com>
2021-05-23 23:36:38 +0200

    fix(main): remove error-prone findSystemLocale call

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,13 @@ import 'package:cabin_booking/app_styles.dart';
 import 'package:cabin_booking/model/cabin_manager.dart';
 import 'package:cabin_booking/model/day_handler.dart';
 import 'package:cabin_booking/widgets/pages/home_page.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:intl/intl.dart';
-import 'package:intl/intl_standalone.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
-  if (kIsWeb) await findSystemLocale();
-
   runApp(
     MultiProvider(
       providers: [
