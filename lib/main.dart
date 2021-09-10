b8e863bc61011f2270ef9470cf321f9f2c0c6156
Albert Mañosa <albertmasa2@gmail.com>
2021-09-10 13:55:21 +0200

    refactor: sort directives alphabetically

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'dart:io' show Platform;
 
-import 'package:cabin_booking/widgets/cabin_booking_app.dart';
 import 'package:cabin_booking/model/cabin_manager.dart';
 import 'package:cabin_booking/model/day_handler.dart';
+import 'package:cabin_booking/widgets/cabin_booking_app.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:window_size/window_size.dart';
