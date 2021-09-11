9b1abbb25666d0cc2de9c6e4c2c376c413823321
Albert Mañosa <albertmasa2@gmail.com>
2021-09-12 01:34:48 +0200

    perf(pages): use AutomaticKeepAliveClientMixin

--- lib/widgets/pages/school_years_page.dart
+++ lib/widgets/pages/school_years_page.dart
@@ -1,11 +1,22 @@
 import 'package:cabin_booking/widgets/school_year/school_years_table.dart';
 import 'package:flutter/material.dart';
 
-class SchoolYearsPage extends StatelessWidget {
+class SchoolYearsPage extends StatefulWidget {
   const SchoolYearsPage({Key? key}) : super(key: key);
 
   @override
+  State<SchoolYearsPage> createState() => _SchoolYearsPageState();
+}
+
+class _SchoolYearsPageState extends State<SchoolYearsPage>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  bool get wantKeepAlive => true;
+
+  @override
   Widget build(BuildContext context) {
+    super.build(context);
+
     return const SchoolYearsTable();
   }
 }
