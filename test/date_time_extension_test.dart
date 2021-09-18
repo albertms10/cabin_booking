e962aef870432cb60d0bc15f7759e19299996d5c
Albert Mañosa <albertmasa2@gmail.com>
2021-09-18 02:00:08 +0200

    test(date_time_extension): add the rest of method tests

--- test/date_time_extension_test.dart
+++ test/date_time_extension_test.dart
@@ -1,7 +1,33 @@
 import 'package:cabin_booking/utils/date_time_extension.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  group('dateOnly', () {
+    test('should return only the date information of this DateTime', () {
+      expect(DateTime(2021, 9, 7, 21, 30, 10).dateOnly, DateTime(2021, 9, 7));
+    });
+  });
+
+  group('addTimeOfDay', () {
+    test('should return a new DateTime adding the TimeOfDay information', () {
+      expect(
+        DateTime(2021, 9, 7)
+            .addTimeOfDay(const TimeOfDay(hour: 21, minute: 30)),
+        DateTime(2021, 9, 7, 21, 30),
+      );
+    });
+
+    test('should return a new DateTime replacing the TimeOfDay information',
+        () {
+      expect(
+        DateTime(2021, 9, 7, 8, 15)
+            .addTimeOfDay(const TimeOfDay(hour: 21, minute: 30)),
+        DateTime(2021, 9, 7, 21, 30),
+      );
+    });
+  });
+
   group('isSameDateAs', () {
     test('should return true when the dates are the same', () {
       expect(
@@ -16,6 +42,32 @@ void main() {
         DateTime(2021, 1, 10, 9, 30).isSameDateAs(DateTime(2021, 2, 10, 9, 30)),
         isFalse,
       );
+    });
+  });
+
+  group('firstDayOfWeek', () {
+    test(
+        'should return the first day of the week relative to this DateTime, '
+        'whose weekday are the same', () {
+      expect(
+        DateTime(2021, 1, 10, 9, 30).firstDayOfWeek.weekday,
+        DateTime(2021, 1, 4).weekday,
+      );
+    });
+
+    test(
+        'should return the first day of the week relative to this DateTime '
+        'while preserving time information', () {
+      expect(
+        DateTime(2021, 1, 10, 9, 30).firstDayOfWeek,
+        DateTime(2021, 1, 4, 9, 30),
+      );
+    });
+  });
+
+  group('toDouble', () {
+    test('should return a double representation of this DateTime', () {
+      expect(DateTime(2021, 1, 10, 9, 30).toDouble(), closeTo(18637.35, 0.01));
     });
   });
 }
