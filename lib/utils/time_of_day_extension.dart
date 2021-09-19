f996154da24e016c828ce365c855cdf54142a82a
Albert Mañosa <albertmasa2@gmail.com>
2021-09-19 21:32:00 +0200

    refactor(time_of_day_extension): rewrite parse and tryParse methods

--- lib/utils/time_of_day_extension.dart
+++ lib/utils/time_of_day_extension.dart
@@ -4,20 +4,45 @@ import 'package:flutter/material.dart';
 extension TimeOfDayExtension on TimeOfDay {
   /// Constructs a new [TimeOfDay] instance based on [formattedString].
   ///
-  /// Returns null when a [FormatException] would be thrown.
-  static TimeOfDay? tryParse(String formattedString) {
-    if (formattedString.isEmpty) return null;
-
+  /// Throws a [FormatException] if the input string cannot be parsed.
+  ///
+  /// The [formattedString] must be a valid non-empty expression of integer
+  /// hours (0..23) and minutes (0..59), separated with ':' or '.'.
+  ///
+  /// Instead of throwing and immediately catching the [FormatException],
+  /// instead use [tryParse] to handle a parsing error.
+  /// Example:
+  /// ```dart
+  /// final value = TimeOfDayExtension.tryParse(formattedString);
+  /// if (value == null) ... handle the problem
+  /// ```
+  static TimeOfDay parse(String formattedString) {
     final particles = formattedString.split(RegExp('[:.]'));
-    if (particles.length < 2) return null;
+    if (particles.length < 2) {
+      throw FormatException('Invalid time format', formattedString);
+    }
 
-    final hour = int.tryParse(particles[0]);
-    if (hour == null) return null;
+    final hour = int.parse(particles.first);
+    final minute = int.parse(particles[1]);
 
-    final minute = int.tryParse(particles[1]);
-    if (minute == null) return null;
+    if ((hour >= 0 && hour < TimeOfDay.hoursPerDay) &&
+        (minute >= 0 && minute < TimeOfDay.minutesPerHour)) {
+      throw FormatException('Time out of range', formattedString);
+    }
 
     return TimeOfDay(hour: hour, minute: minute);
+  }
+
+  /// Constructs a new [TimeOfDay] instance based on [formattedString].
+  ///
+  /// Works like [parse] except that this function returns `null` where [parse]
+  /// would throw a [FormatException].
+  static TimeOfDay? tryParse(String formattedString) {
+    try {
+      return parse(formattedString);
+    } on FormatException {
+      return null;
+    }
   }
 
   static int compare(TimeOfDay a, TimeOfDay b) => a.compareTo(b);
