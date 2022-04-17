a0788dd2ccb367b05fa2f7bd97be2d03c6de811d
Albert Mañosa <albertmasa2@gmail.com>
2022-04-17 23:35:29 +0200

    feat(show_search_bar_intent): implement search bar shortcut

--- lib/widgets/cabin_booking_app.dart
+++ lib/widgets/cabin_booking_app.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
+
 import 'package:cabin_booking/app_styles.dart' as app_styles;
+import 'package:cabin_booking/services/show_search_bar_intent.dart';
 import 'package:cabin_booking/widgets/pages/home_page.dart';
 import 'package:cabin_booking/widgets/pages/main_data_loader.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:intl/intl.dart';
 
@@ -26,8 +30,39 @@ class CabinBookingApp extends StatelessWidget {
           child: child!,
         );
       },
-      home: const MainDataLoader(
-        child: HomePage(),
+      home: const _ActionableFocusedShortcuts(
+        child: MainDataLoader(
+          child: HomePage(),
+        ),
+      ),
+    );
+  }
+}
+
+class _ActionableFocusedShortcuts extends StatelessWidget {
+  final Widget child;
+
+  const _ActionableFocusedShortcuts({Key? key, required this.child})
+      : super(key: key);
+
+  LogicalKeySet get _searchBarLogicalKeySet => Platform.isMacOS
+      ? LogicalKeySet(LogicalKeyboardKey.meta, LogicalKeyboardKey.keyK)
+      : LogicalKeySet(LogicalKeyboardKey.control, LogicalKeyboardKey.keyK);
+
+  @override
+  Widget build(BuildContext context) {
+    return Shortcuts(
+      shortcuts: {
+        _searchBarLogicalKeySet: const ShowSearchBarIntent(),
+      },
+      child: Actions(
+        actions: {
+          ShowSearchBarIntent: ShowSearchBarAction(context),
+        },
+        child: Focus(
+          autofocus: true,
+          child: child,
+        ),
       ),
     );
   }
