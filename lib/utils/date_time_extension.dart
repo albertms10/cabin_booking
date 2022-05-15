27a72c56da62a53ff1dfcdbbdb742440f8ab0bc3
Albert Mañosa <albertmasa2@gmail.com>
2022-05-15 14:42:12 +0200

    feat(date_time_extension): add `formatRelative` method

--- lib/utils/date_time_extension.dart
+++ lib/utils/date_time_extension.dart
@@ -1,5 +1,6 @@
 import 'package:cabin_booking/utils/time_of_day_extension.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:intl/intl.dart';
 
 extension DateTimeExtension on DateTime {
@@ -17,4 +18,36 @@ extension DateTimeExtension on DateTime {
       weekday == 1 ? this : subtract(Duration(days: weekday - 1));
 
   double toDouble() => millisecondsSinceEpoch / Duration.millisecondsPerDay;
+
+  String _formatRelativeDate(AppLocalizations appLocalizations) {
+    final now = DateTime.now();
+    if (isSameDateAs(now)) return appLocalizations.today;
+
+    final yesterday = now.subtract(const Duration(days: 1));
+    if (isSameDateAs(yesterday)) return appLocalizations.yesterday;
+
+    final tomorrow = now.add(const Duration(days: 1));
+    if (isSameDateAs(tomorrow)) return appLocalizations.tomorrow;
+
+    return DateFormat.yMMMEd().format(this);
+  }
+
+  String _formatRelativeTime() => DateFormat.Hm().format(this);
+
+  /// Returns a relatively formatted [String] from this [DateTime].
+  ///
+  /// Example:
+  /// ```dart
+  /// final today = DateTime.now();
+  /// assert(today.formatRelative(appLocalizations) == 'Today, 09:00');
+  ///
+  /// final yesterday = today.subtract(const Duration(days: 1));
+  /// assert(yesterday.formatRelative(appLocalizations) == 'Yesterday, 09:00');
+  /// ```
+  String formatRelative(AppLocalizations appLocalizations) {
+    final date = _formatRelativeDate(appLocalizations);
+    final time = _formatRelativeTime();
+
+    return [date, time].join(', ');
+  }
 }
