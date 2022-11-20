20f674cd967c569ff58adb6b614bc6519dcc7ef8
Albert Mañosa <albertmasa2@gmail.com>
2022-11-20 19:30:54 +0100

    feat: remove booking status (#83)

--- lib/widgets/booking/booking_preview_panel.dart
+++ lib/widgets/booking/booking_preview_panel.dart
@@ -2,7 +2,6 @@ import 'package:cabin_booking/model.dart';
 import 'package:cabin_booking/widgets/booking/booking_preview_panel_action_bar.dart';
 import 'package:cabin_booking/widgets/booking/booking_preview_panel_row.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:intl/intl.dart';
 
 class BookingPreviewPanel extends StatelessWidget {
@@ -19,8 +18,6 @@ class BookingPreviewPanel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final appLocalizations = AppLocalizations.of(context)!;
-
     return Padding(
       padding: const EdgeInsets.all(4),
       child: Column(
@@ -32,11 +29,6 @@ class BookingPreviewPanel extends StatelessWidget {
             onClose: onClose,
           ),
           _BookingPreviewPanelHeadline(booking: booking),
-          const SizedBox(height: 28),
-          BookingPreviewPanelRow(
-            trailing: Icon(booking.status.icon),
-            child: Text(booking.status.localized(appLocalizations)),
-          ),
           const SizedBox(height: 32),
         ],
       ),
