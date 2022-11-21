6ba69c34dc7f844165bdd9fd1edb6429152531c2
Albert Mañosa <albertmasa2@gmail.com>
2022-11-22 00:47:08 +0100

    test(utils): add tests for `MapExtension`

--- test/utils/main.dart
+++ test/utils/main.dart
@@ -2,6 +2,7 @@ import 'color_extension_test.dart' as color_extension_test;
 import 'date_time_extension_test.dart' as date_time_extension_test;
 import 'iterable_extension_test.dart' as iterable_extension_test;
 import 'iterable_string_extension_test.dart' as iterable_string_extension_test;
+import 'map_extension_test.dart' as map_extension_test;
 import 'num_extension_test.dart' as num_extension_test;
 import 'reg_exp_match_extension_test.dart' as reg_exp_match_extension_test;
 import 'string_extension_test.dart' as string_extension_test;
@@ -12,6 +13,7 @@ void main() {
   date_time_extension_test.main();
   iterable_extension_test.main();
   iterable_string_extension_test.main();
+  map_extension_test.main();
   num_extension_test.main();
   reg_exp_match_extension_test.main();
   string_extension_test.main();
