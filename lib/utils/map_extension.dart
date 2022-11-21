376aa49bb270a1db44f7d20f8fa9c0de256dad24
Albert Mañosa <albertmasa2@gmail.com>
2022-11-22 00:41:27 +0100

    docs(utils): document missing methods

--- lib/utils/map_extension.dart
+++ lib/utils/map_extension.dart
@@ -1,4 +1,19 @@
+/// Map extension.
 extension MapExtension<K, V> on Map<K, V> {
+  /// Returns a new [Map] filled with the result of [ifAbsent] for [keys] that
+  /// are not present in this [Map].
+  ///
+  /// Unlike [Map.update], this method does not mutate this [Map].
+  ///
+  /// Example:
+  /// ```dart
+  /// const map = <int, bool?>{1: true, 2: false};
+  /// final filled = map.fillEmptyKeyValues(
+  ///   keys: const [0, 1, 2, 5],
+  ///   ifAbsent: () => null,
+  /// );
+  /// assert(filled == const {0: null, 1: true, 2: false, 5: null});
+  /// ```
   Map<K, V> fillEmptyKeyValues({
     Iterable<K> keys = const [],
     required V Function() ifAbsent,
