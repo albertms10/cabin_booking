87722ecd63b2a207d367dbd85750cb1062f603f8
Albert Mañosa <albertmasa2@gmail.com>
2022-12-03 01:33:43 +0100

    feat(string_extension): add `matchesWith` method (#109)
    * feat(string_extension): add `matchesWith` method
    
    * test(string_extension): test `matchesWith`

--- test/utils/string_extension_test.dart
+++ test/utils/string_extension_test.dart
@@ -23,5 +23,22 @@ void main() {
         );
       });
     });
+
+    group('.matchesWith', () {
+      test('should return true if any matcher matches this String', () {
+        expect('m'.matchesWith(['ma', 'math', '', '34']), isTrue);
+        expect('mAt'.matchesWith(['MaTCH']), isTrue);
+        expect('alpha'.matchesWith(['The alphabet']), isTrue);
+        expect('alpha: the'.matchesWith(['The, alphabet']), isTrue);
+        expect('1 3'.matchesWith(['1-3']), isTrue);
+        expect('1 3 2'.matchesWith([r'1  ;·$·$2%&/( 3']), isTrue);
+        expect(r'1  ;·$·$2%&/( 3'.matchesWith(['1 2 3']), isTrue);
+      });
+
+      test('should return false if the matchers do not match this String', () {
+        expect('mat'.matchesWith(['', 'tree', 'mars']), isFalse);
+        expect('the name is'.matchesWith(['the is']), isFalse);
+      });
+    });
   });
 }
