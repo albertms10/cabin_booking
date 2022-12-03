b524ff625ff6648741800dfc00c8590145f692b6
Albert Mañosa <albertmasa2@gmail.com>
2022-12-03 01:30:53 +0100

    feat(string_extension): add `matchesWith` method

--- lib/utils/string_extension.dart
+++ lib/utils/string_extension.dart
@@ -1,4 +1,5 @@
 import 'package:cabin_booking/utils/reg_exp_match_extension.dart';
+import 'package:collection/collection.dart';
 
 /// String extension.
 extension StringExtension on String {
@@ -27,5 +28,27 @@ extension StringExtension on String {
     }
 
     return namedGroups;
+  }
+
+  /// Returns `true` if any matcher matches this query [String].
+  ///
+  /// Examples:
+  /// ```dart
+  /// assert('mAt'.matchesWith(['MaTCH']));
+  /// assert('alpha'.matchesWith(['The alphabet']));
+  /// assert('1 3'.matchesWith(['1-3']));
+  /// ```
+  bool matchesWith(Iterable<String?> matchers) {
+    final nullSafeMatchers =
+        matchers.whereNotNull().map((matcher) => matcher.toLowerCase());
+
+    return RegExp(r'[\w\d]+')
+        .allMatches(toLowerCase())
+        .map((match) => match[0])
+        .whereNotNull()
+        .every(
+          (fragment) =>
+              nullSafeMatchers.any((matcher) => matcher.contains(fragment)),
+        );
   }
 }
