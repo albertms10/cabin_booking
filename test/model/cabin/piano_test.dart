90463d39ecb2aa7e6f78491e5d846750b5cb33cf
Albert Mañosa <albertmasa2@gmail.com>
2022-12-04 23:17:41 +0100

    test(cabin): add tests for Cabin classes

---
+++ test/model/cabin/piano_test.dart
@@ -0,0 +1,31 @@
+import 'package:cabin_booking/model.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Piano', () {
+    group('.from()', () {
+      test('should create a new Piano from a JSON object', () {
+        const rawPiano = {
+          'b': 'Yamaha',
+          'm': 'C5',
+          'ie': false,
+        };
+        final piano = Piano.from(rawPiano);
+        expect(piano, const Piano(brand: 'Yamaha', model: 'C5'));
+      });
+    });
+
+    group('.toJson()', () {
+      test('should return a JSON object representation of this Piano', () {
+        const piano = Piano(brand: 'Yamaha', model: 'C5');
+        const rawPiano = {
+          'b': 'Yamaha',
+          'm': 'C5',
+          'ie': false,
+        };
+        expect(piano.toJson(), rawPiano);
+        expect(Piano.from(piano.toJson()), piano);
+      });
+    });
+  });
+}
