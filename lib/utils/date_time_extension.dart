885c6d1f56c874257a15d236c3d31a42edeae421
Albert Mañosa <albertmasa2@gmail.com>
2022-12-04 16:59:49 +0100

    refactor(date_time_extension): remove unused import

--- lib/utils/date_time_extension.dart
+++ lib/utils/date_time_extension.dart
@@ -1,4 +1,3 @@
-import 'package:cabin_booking/utils/time_of_day_extension.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:intl/intl.dart';
