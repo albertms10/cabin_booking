f4760d23faf92735372ca241e7d3748455928390
Albert Mañosa <albertmasa2@gmail.com>
2022-12-09 00:21:29 +0100

    test(recurring_booking): provide both recurring methods

--- test/model/booking/recurring_booking_test.dart
+++ test/model/booking/recurring_booking_test.dart
@@ -34,7 +34,32 @@ void main() {
 
     group('.toJson()', () {
       test(
-        'should return a JSON object representation of this RecurringBooking',
+        'should return a JSON object representation of this RecurringBooking '
+        '(endDate method)',
+        () {
+          const rawRecurringBooking = {
+            'id': 'booking-id',
+            'cd': '1969-07-20T20:18:04.000Z',
+            'md': '1969-07-20T20:18:04.000Z',
+            'mc': 1,
+            'sd': '2022-12-04T09:00:00.000Z',
+            'ed': '2022-12-04T10:30:00.000Z',
+            'de': 'Student',
+            'il': false,
+            'p': 1,
+            're': 1,
+            'red': '2023-02-04T00:00:00.000Z',
+          };
+          expect(
+            RecurringBooking.from(rawRecurringBooking).toJson(),
+            rawRecurringBooking,
+          );
+        },
+      );
+
+      test(
+        'should return a JSON object representation of this RecurringBooking '
+        '(occurrences method)',
         () {
           const rawRecurringBooking = {
             'id': 'booking-id',
