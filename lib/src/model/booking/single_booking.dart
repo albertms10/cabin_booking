albertms10/cabin_booking

commit 796d403b390cecf433aa1eaaf3ef1afb23690d89

refactor: rename `startDateTime` → `startDate` and `date` → `dateOnly` (#131)
* refactor: rename `startDateTime` → `startDate` and `date` → `dateOnly`

* refactor(item): rename DateTime JSON fields

lib/src/model/booking/single_booking.dart
@@ -4,8 +4,8 @@ class SingleBooking extends Booking {
   SingleBooking({
     super.id,
     super.description,
-    super.startDateTime,
-    super.endDateTime,
+    super.startDate,
+    super.endDate,
     super.isLocked = false,
     super.cabinId,
     super.recurringBookingId,
@@ -19,8 +19,8 @@ class SingleBooking extends Booking {
       : super(
           id: booking.id,
           description: booking.description,
-          startDateTime: booking.startDateTime,
-          endDateTime: booking.endDateTime,
+          startDate: booking.startDate,
+          endDate: booking.endDate,
           isLocked: booking.isLocked,
           cabinId: booking.cabinId,
         );
@@ -29,16 +29,16 @@ class SingleBooking extends Booking {
   SingleBooking copyWith({
     String? id,
     String? description,
-    DateTime? startDateTime,
-    DateTime? endDateTime,
+    DateTime? startDate,
+    DateTime? endDate,
     bool? isLocked,
     String? cabinId,
   }) =>
       SingleBooking(
         id: id ?? super.id,
         description: description ?? this.description,
-        startDateTime: startDateTime ?? this.startDateTime,
-        endDateTime: endDateTime ?? this.endDateTime,
+        startDate: startDate ?? startDate,
+        endDate: endDate ?? endDate,
         isLocked: isLocked ?? this.isLocked,
         cabinId: cabinId ?? this.cabinId,
       );

lib/src/model/booking/booking.dart
@@ -8,15 +8,20 @@ import '../item.dart';
 
 abstract class _JsonFields {
   static const description = 'de';
-  static const startDateTime = 'sd';
-  static const endDateTime = 'ed';
+  static const startDate = 'sd';
+  static const endDate = 'ed';
   static const isLocked = 'il';
 }
 
-abstract class Booking extends Item {
+abstract class Booking extends Item with DateRanger {
   String? description;
-  DateTime? startDateTime;
-  DateTime? endDateTime;
+
+  @override
+  DateTime? startDate;
+
+  @override
+  DateTime? endDate;
+
   bool isLocked;
   String? cabinId;
 
@@ -27,8 +32,8 @@ abstract class Booking extends Item {
   Booking({
     super.id,
     this.description,
-    this.startDateTime,
-    this.endDateTime,
+    this.startDate,
+    this.endDate,
     this.isLocked = false,
     this.cabinId,
     this.recurringBookingId,
@@ -38,11 +43,11 @@ abstract class Booking extends Item {
 
   Booking.from(super.other)
       : description = other[_JsonFields.description] as String?,
-        startDateTime = DateTime.tryParse(
-          other[_JsonFields.startDateTime] as String? ?? '',
+        startDate = DateTime.tryParse(
+          other[_JsonFields.startDate] as String? ?? '',
         ),
-        endDateTime =
-            DateTime.tryParse(other[_JsonFields.endDateTime] as String? ?? ''),
+        endDate =
+            DateTime.tryParse(other[_JsonFields.endDate] as String? ?? ''),
         isLocked = other[_JsonFields.isLocked] as bool,
         super.from();
 
@@ -50,19 +55,17 @@ abstract class Booking extends Item {
   Map<String, dynamic> toJson() => {
         ...super.toJson(),
         _JsonFields.description: description,
-        _JsonFields.startDateTime: startDateTime?.toUtc().toIso8601String(),
-        _JsonFields.endDateTime: endDateTime?.toUtc().toIso8601String(),
+        _JsonFields.startDate: startDate?.toUtc().toIso8601String(),
+        _JsonFields.endDate: endDate?.toUtc().toIso8601String(),
         _JsonFields.isLocked: isLocked,
       };
 
-  /// Date only part of [startDateTime].
-  DateTime? get date => startDateTime?.dateOnly;
+  /// Date only part of [startDate].
+  DateTime? get dateOnly => startDate?.dateOnly;
 
-  TimeOfDay get startTime => TimeOfDay.fromDateTime(startDateTime!.toLocal());
+  TimeOfDay get startTime => TimeOfDay.fromDateTime(startDate!.toLocal());
 
-  TimeOfDay get endTime => TimeOfDay.fromDateTime(endDateTime!.toLocal());
-
-  Duration get duration => endDateTime!.difference(startDateTime!);
+  TimeOfDay get endTime => TimeOfDay.fromDateTime(endDate!.toLocal());
 
   Map<TimeOfDay, Duration> get hoursSpan {
     final timeRanges = <TimeOfDay, Duration>{};
@@ -96,23 +99,22 @@ abstract class Booking extends Item {
       '–${endTime.format24Hour()}';
 
   String get dateTimeRange =>
-      '${DateFormat.yMd().format(startDateTime!)} $timeRange';
+      '${DateFormat.yMd().format(dateOnly!)} $timeRange';
 
-  bool isOn(DateTime dateTime) =>
-      startDateTime?.isSameDateAs(dateTime) ?? false;
+  bool isOn(DateTime dateTime) => dateOnly?.isSameDateAs(dateTime) ?? false;
 
-  bool isBetween(DateRanger dateRange) => dateRange.includes(startDateTime!);
+  bool isBetween(DateRanger dateRange) => dateRange.includes(startDate!);
 
   bool collidesWith(Booking booking) =>
-      startDateTime!.isBefore(booking.endDateTime!) &&
-      endDateTime!.isAfter(booking.startDateTime!);
+      startDate!.isBefore(booking.endDate!) &&
+      endDate!.isAfter(booking.startDate!);
 
   @override
   Booking copyWith({
     String? id,
     String? description,
-    DateTime? startDateTime,
-    DateTime? endDateTime,
+    DateTime? startDate,
+    DateTime? endDate,
     bool? isLocked,
     String? cabinId,
   });
@@ -120,8 +122,8 @@ abstract class Booking extends Item {
   @override
   void replaceWith(covariant Booking item) {
     description = item.description;
-    startDateTime = item.startDateTime;
-    endDateTime = item.endDateTime;
+    startDate = item.startDate;
+    endDate = item.endDate;
     isLocked = item.isLocked;
 
     super.replaceWith(item);
@@ -133,5 +135,5 @@ abstract class Booking extends Item {
 
   @override
   int compareTo(covariant Booking other) =>
-      startDateTime!.compareTo(other.startDateTime!);
+      startDate!.compareTo(other.startDate!);
 }

lib/src/model/booking/booking_manager.dart
@@ -94,9 +94,9 @@ class BookingManager with ChangeNotifier {
       });
 
   bool bookingsCollideWith(Booking booking) {
-    if (booking.date == null) return false;
+    if (booking.dateOnly == null) return false;
 
-    return allBookingsOn(booking.date!)
+    return allBookingsOn(booking.dateOnly!)
             .where(
               (comparingBooking) =>
                   (comparingBooking.recurringBookingId == null ||
@@ -178,11 +178,13 @@ class BookingManager with ChangeNotifier {
 
     for (final booking in bookingsList) {
       final shouldAddDate = dates.firstWhereOrNull(
-            (date) => booking.date != null && date.isSameDateAs(booking.date!),
+            (date) =>
+                booking.dateOnly != null &&
+                date.isSameDateAs(booking.dateOnly!),
           ) !=
           null;
 
-      if (!shouldAddDate) dates.add(booking.date!);
+      if (!shouldAddDate) dates.add(booking.dateOnly!);
     }
 
     return dates;
@@ -193,7 +195,7 @@ class BookingManager with ChangeNotifier {
 
     for (final booking in allBookings) {
       bookingsPerDay.update(
-        booking.date!,
+        booking.dateOnly!,
         (count) => count + 1,
         ifAbsent: () => 1,
       );
@@ -206,10 +208,10 @@ class BookingManager with ChangeNotifier {
     final bookingsPerDay = SplayTreeMap<DateTime, Duration>();
 
     for (final booking in allBookings) {
-      if (dateRange != null && !dateRange.includes(booking.date!)) continue;
+      if (dateRange != null && !dateRange.includes(booking.dateOnly!)) continue;
 
       bookingsPerDay.update(
-        booking.date!.firstDayOfWeek,
+        booking.dateOnly!.firstDayOfWeek,
         (duration) => duration + booking.duration,
         ifAbsent: () => booking.duration,
       );

lib/src/model/booking/recurring_booking.dart
@@ -20,8 +20,8 @@ class RecurringBooking extends Booking {
   RecurringBooking({
     super.id,
     super.description,
-    super.startDateTime,
-    super.endDateTime,
+    super.startDate,
+    super.endDate,
     super.isLocked,
     super.cabinId,
     this.periodicity = Periodicity.weekly,
@@ -74,8 +74,8 @@ class RecurringBooking extends Booking {
     return RecurringBooking(
       id: booking.id,
       description: booking.description,
-      startDateTime: booking.startDateTime,
-      endDateTime: booking.endDateTime,
+      startDate: booking.startDate,
+      endDate: booking.endDate,
       isLocked: booking.isLocked,
       cabinId: booking.cabinId,
       periodicity: periodicity,
@@ -112,7 +112,7 @@ class RecurringBooking extends Booking {
 
     assert(_occurrences != null, '_occurrences must be non-null.');
 
-    return date!.add(periodicityDuration * _occurrences!);
+    return dateOnly!.add(periodicityDuration * _occurrences!);
   }
 
   set recurringEndDate(DateTime date) {
@@ -126,7 +126,7 @@ class RecurringBooking extends Booking {
     assert(_recurringEndDate != null, '_recurringEndDate must be non-null.');
 
     var count = 0;
-    var runDate = startDateTime!;
+    var runDate = startDate!;
 
     while (runDate.isBefore(_recurringEndDate!)) {
       runDate = runDate.add(periodicityDuration);
@@ -144,8 +144,8 @@ class RecurringBooking extends Booking {
   SingleBooking asSingleBooking({bool linked = true}) => SingleBooking(
         id: linked ? '$id-0' : (recurringBookingId ?? id),
         description: description,
-        startDateTime: startDateTime,
-        endDateTime: endDateTime,
+        startDate: startDate,
+        endDate: endDate,
         isLocked: isLocked,
         cabinId: cabinId,
         recurringBookingId: linked ? id : null,
@@ -153,7 +153,7 @@ class RecurringBooking extends Booking {
 
   List<SingleBooking> get bookings {
     final runBookings = <SingleBooking>[];
-    var runDate = startDateTime!;
+    var runDate = startDate!;
     var movedBooking = asSingleBooking();
 
     var count = 1;
@@ -171,8 +171,8 @@ class RecurringBooking extends Booking {
 
       if (runDate.isBefore(recurringEndDate)) {
         movedBooking = movedBooking.copyWith(
-          startDateTime: runDate,
-          endDateTime: runDate.add(duration),
+          startDate: runDate,
+          endDate: runDate.add(duration),
         );
         count++;
       }
@@ -191,8 +191,8 @@ class RecurringBooking extends Booking {
   RecurringBooking copyWith({
     String? id,
     String? description,
-    DateTime? startDateTime,
-    DateTime? endDateTime,
+    DateTime? startDate,
+    DateTime? endDate,
     bool? isLocked,
     String? cabinId,
     Periodicity? periodicity,
@@ -203,8 +203,8 @@ class RecurringBooking extends Booking {
       RecurringBooking(
         id: id ?? this.id,
         description: description ?? this.description,
-        startDateTime: startDateTime ?? this.startDateTime,
-        endDateTime: endDateTime ?? this.endDateTime,
+        startDate: startDate ?? startDate,
+        endDate: endDate ?? endDate,
         isLocked: isLocked ?? this.isLocked,
         cabinId: cabinId ?? this.cabinId,
         periodicity: periodicity ?? this.periodicity,

lib/src/model/booking/tokenized_booking.dart
@@ -148,8 +148,8 @@ class TokenizedBooking {
     }
 
     return SingleBooking(
-      startDateTime: now.addTimeOfDay(startTime),
-      endDateTime: now.addTimeOfDay(endTime),
+      startDate: now.addTimeOfDay(startTime),
+      endDate: now.addTimeOfDay(endTime),
     );
   }
 

lib/src/model/cabin/cabin_manager.dart
@@ -357,7 +357,7 @@ class CabinManager extends WritableManager<Set<Cabin>> with ChangeNotifier {
             ...cabin.searchBookings(query, limit: perCabinLimit),
         ],
         // Descending.
-        (a, b) => b.startDateTime!.compareTo(a.startDateTime!),
+        (a, b) => b.startDate!.compareTo(a.startDate!),
       );
 
   Set<Cabin> get _defaultCabins => SplayTreeSet();

lib/src/model/date/date_range.dart
@@ -37,11 +37,11 @@ class DateRange with DateRanger {
   /// assert(dateRange.endDate == DateTime(2022, 12, 5));
   /// ```
   factory DateRange.from(DateTime dateTime) {
-    final startDate = dateTime.dateOnly;
+    final startDateOnly = dateTime.dateOnly;
 
     return DateRange(
-      startDate: startDate,
-      endDate: startDate.add(const Duration(days: 1)),
+      startDate: startDateOnly,
+      endDate: startDateOnly.add(const Duration(days: 1)),
     );
   }
 

lib/src/model/item.dart
@@ -5,8 +5,8 @@ import 'serializable.dart';
 
 abstract class _JsonFields {
   static const id = 'id';
-  static const creationDateTime = 'cdt';
-  static const modificationDateTime = 'mdt';
+  static const creationDateTime = 'cd';
+  static const modificationDateTime = 'md';
   static const modificationCount = 'mc';
 }
 

commit cfa492cbbcdde09b0fa1f0b5e4fee193bad754f1

fix(single_booking): use correct `copyWith` default values
Addresses an issue introduced in #131

Relates to #162

lib/src/model/booking/single_booking.dart
@@ -35,8 +35,8 @@ class SingleBooking extends Booking {
   }) =>
       SingleBooking(
         id: id ?? super.id,
-        startDate: startDate ?? startDate,
-        endDate: endDate ?? endDate,
+        startDate: startDate ?? this.startDate,
+        endDate: endDate ?? this.endDate,
         description: description ?? this.description,
         isLocked: isLocked ?? this.isLocked,
         cabinId: cabinId ?? this.cabinId,