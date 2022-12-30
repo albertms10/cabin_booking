75090e29a09400bc1b49dc7d91823703adf92003
Albert Mañosa <albertmasa2@gmail.com>
2022-12-30 21:33:57 +0100

    fix(booking_preview_panel_action_bar): pass a copy of the booking object to `BookingDialog` (#167)

--- lib/widgets/booking/booking_preview_panel_action_bar.dart
+++ lib/widgets/booking/booking_preview_panel_action_bar.dart
@@ -89,9 +89,9 @@ class _BookingPreviewEditIconButton extends StatelessWidget {
       context: context,
       builder: (context) => BookingDialog(
         booking: (booking.recurringBooking != null
-            ? booking.recurringBooking!
-            : booking)
-          ..cabinId = cabin.id,
+                ? booking.recurringBooking!
+                : booking)
+            .copyWith(cabinId: cabin.id),
       ),
     );
 
