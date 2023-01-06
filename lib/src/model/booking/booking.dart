albertms10/cabin_booking

commit a46353b5b914c4d5342548ba59953e8bc5781c84

feat(date_ranger): implement new `textualDateTime` methods (#168)
* feat(date_ranger): implement new `textualDateTime` methods

* refactor(booking): replace old `dateTimeRange` methods

* refactor(time_of_day_extension): remove unused `format24Hour` method

* test(date_ranger): add tests for `textualDateTime` methods

* fix(date_range): ensure working with local `DateTime`

* test(date_ranger): fix UTC to local test case

lib/src/model/booking/booking.dart
@@ -1,6 +1,4 @@
 import 'package:cabin_booking/utils/date_time_extension.dart';
-import 'package:cabin_booking/utils/time_of_day_extension.dart';
-import 'package:intl/intl.dart';
 
 import '../cabin/cabin.dart';
 import '../date/date_range_item.dart';
@@ -58,12 +56,6 @@ abstract class Booking extends DateRangeItem {
   /// Date only part of [startDate].
   DateTime? get dateOnly => startDate?.dateOnly;
 
-  String get timeRange => '${startTime!.format24Hour()}'
-      '–${endTime!.format24Hour()}';
-
-  String get dateTimeRange =>
-      '${DateFormat.yMd().format(dateOnly!)} $timeRange';
-
   @override
   Booking copyWith({
     String? id,
@@ -86,7 +78,7 @@ abstract class Booking extends DateRangeItem {
 
   @override
   String toString() =>
-      [if (isLocked) '🔒', description, dateTimeRange].join(' ');
+      [if (isLocked) '🔒', description, textualDateTime].join(' ');
 
   @override
   bool operator ==(Object other) =>

lib/src/model/date/date_ranger.dart
@@ -1,6 +1,9 @@
 import 'package:cabin_booking/utils/date_time_extension.dart';
 import 'package:cabin_booking/utils/time_of_day_extension.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+typedef DateFormatter = DateFormat Function(DateFormat);
 
 /// Adds date range operations between a [startDate] and an [endDate].
 mixin DateRanger {
@@ -139,6 +142,99 @@ mixin DateRanger {
 
     return endDate!.difference(startDate!);
   }
+
+  /// Returns the formatted local date and time range of this [DateRanger].
+  ///
+  /// By default, [referenceDateTime] is set to [DateTime.now()] to whether
+  /// show or hide the matching year.
+  ///
+  /// Example:
+  /// ```dart
+  /// final dateRange = DateRange(
+  ///   startDate: DateTime(2022, 12, 1, 9, 30),
+  ///   endDate: DateTime(2022, 12, 1, 21, 30),
+  /// );
+  /// final textualDateTime = dateRange.textualDateTime(
+  ///   referenceDateTime: DateTime(2022),
+  /// );
+  /// assert(textualDateTime == '1 December 9:30–21:30');
+  /// ```
+  ///
+  /// Date format may also be overridden as desired:
+  ///
+  /// ```dart
+  /// final dateRange = DateRange(
+  ///   startDate: DateTime(2022, 12, 1, 9, 30),
+  ///   endDate: DateTime(2022, 12, 31, 21, 30),
+  /// );
+  /// final formatted = dateRange.textualDateTime(
+  ///   referenceDateTime: DateTime(2023),
+  ///   fullDateFormat: (format) => format.add_yMMMEd(),
+  ///   monthDayFormat: (format) => format.add_LLL().add_d(),
+  ///   timeFormat: (format) => format.add_Hms(),
+  /// );
+  /// assert(formatted == 'Thu, Dec 1, 2022 09:30:00 – Dec 31 21:30:00');
+  /// ```
+  String textualDateTime({
+    DateTime? referenceDateTime,
+    DateFormatter? timeFormat,
+    DateFormatter? monthDayFormat,
+    DateFormatter? fullDateFormat,
+  }) {
+    if (hasInfiniteStart && hasInfiniteEnd) return _formatTextualDateTime();
+
+    timeFormat ??= (format) => format.add_Hm();
+    fullDateFormat ??= (format) => format.add_yMMMMd();
+    final formatFull = timeFormat(fullDateFormat(DateFormat()));
+
+    if (startDate?.year != endDate?.year) {
+      return _formatTextualDateTime(start: formatFull, end: formatFull);
+    }
+
+    referenceDateTime ??= DateTime.now();
+    monthDayFormat ??= (format) => format.add_MMMMd();
+    final formatNoYear = timeFormat(monthDayFormat(DateFormat()));
+    final formatTimeOnly = timeFormat(DateFormat());
+
+    return _formatTextualDateTime(
+      start:
+          startDate?.year == referenceDateTime.year ? formatNoYear : formatFull,
+      end: startDate!.isSameDateAs(endDate!) ? formatTimeOnly : formatNoYear,
+    );
+  }
+
+  String _formatTextualDateTime({
+    DateFormat? start,
+    DateFormat? end,
+    String separator = '–',
+  }) {
+    final formattedStartDate = _formatLocalDateTime(startDate, start);
+    final formattedEndDate = _formatLocalDateTime(endDate, end);
+    final shouldWhitespace = formattedStartDate == null ||
+        (formattedEndDate?.contains(RegExp(r'\s')) ?? true);
+
+    return '$formattedStartDate'
+        '${shouldWhitespace ? ' $separator ' : separator}'
+        '$formattedEndDate';
+  }
+
+  String? _formatLocalDateTime(DateTime? dateTime, DateFormat? dateFormat) =>
+      dateTime != null ? dateFormat?.format(dateTime.toLocal()) : null;
+
+  /// Returns the formatted local time range of this [DateRanger].
+  ///
+  /// Example:
+  /// ```dart
+  /// final dateRange = DateRange(
+  ///   startDate: DateTime(2022, 12, 1, 9, 30),
+  ///   endDate: DateTime(2022, 12, 1, 21, 30),
+  /// );
+  /// assert(dateRange.timeRange == '9:30–21:30');
+  /// ```
+  String get textualTime => textualDateTime(
+        monthDayFormat: (format) => format,
+        fullDateFormat: (format) => format,
+      );
 
   /// The time span of this [DateRanger] in hours.
   ///

lib/utils/time_of_day_extension.dart
@@ -107,16 +107,6 @@ extension TimeOfDayExtension on TimeOfDay {
         minutes: minute.roundToNearest(n) - minute,
       );
 
-  /// Returns the 24-hour formatted string representation of this [TimeOfDay],
-  /// separated with ':'.
-  ///
-  /// Examples:
-  /// ```dart
-  /// assert(const TimeOfDay(hour: 9, minute: 00).format24Hour() == '09:00');
-  /// assert(const TimeOfDay(hour: 17, minute: 14).format24Hour() == '17:14');
-  /// ```
-  String format24Hour() => '${hour.padLeft2}:${minute.padLeft2}';
-
   // TODO(albertms10): remove when implemented in Flutter, https://github.com/flutter/flutter/pull/59981.
   static int compare(TimeOfDay a, TimeOfDay b) => a.compareTo(b);
 

lib/widgets/booking/booking_card.dart
@@ -192,7 +192,7 @@ class _BookingCardInfo extends StatelessWidget {
                     ),
                     if (constraints.maxHeight > 30)
                       Text(
-                        booking.timeRange,
+                        booking.textualTime,
                         style: theme.textTheme.caption?.copyWith(
                           fontSize: constraints.maxHeight > 40
                               ? 14

lib/widgets/booking/booking_preview_panel.dart
@@ -69,7 +69,7 @@ class _BookingPreviewPanelHeadline extends StatelessWidget {
               style: TextStyle(color: theme.hintColor),
             ),
           ),
-          Text(booking.timeRange),
+          Text(booking.textualTime),
         ],
       ),
     );

test/model/date/date_ranger_test.dart
@@ -353,6 +353,187 @@ void main() {
       });
     });
 
+    group('.textualDateTime()', () {
+      test(
+        'should return a textual range representation of a finite '
+        'DateRanger with the same year',
+        () {
+          final dateRange = DateRange(
+            startDate: DateTime(2022, 12, 1, 9, 30),
+            endDate: DateTime(2022, 12, 31, 21, 30),
+          );
+          expect(
+            dateRange.textualDateTime(referenceDateTime: DateTime(2022)),
+            'December 1 09:30 – December 31 21:30',
+          );
+          expect(
+            dateRange.textualDateTime(referenceDateTime: DateTime(2023)),
+            'December 1, 2022 09:30 – December 31 21:30',
+          );
+          expect(
+            dateRange.textualDateTime(referenceDateTime: DateTime(2023)),
+            dateRange.textualDateTime(referenceDateTime: DateTime(2021)),
+          );
+          expect(
+            dateRange.textualDateTime(
+              referenceDateTime: DateTime(2023),
+              fullDateFormat: (format) => format.add_yMMMMEEEEd(),
+              monthDayFormat: (format) => format.add_LLL().add_d(),
+              timeFormat: (format) => format.add_Hms(),
+            ),
+            'Thursday, December 1, 2022 09:30:00 – Dec 31 21:30:00',
+          );
+        },
+      );
+
+      test(
+        'should return a textual range representation of a finite '
+        'DateRanger with the same year and day',
+        () {
+          final dateRange = DateRange(
+            startDate: DateTime(2022, 12, 1, 9, 30),
+            endDate: DateTime(2022, 12, 1, 21, 30),
+          );
+          expect(
+            dateRange.textualDateTime(referenceDateTime: DateTime(2022)),
+            'December 1 09:30–21:30',
+          );
+          expect(
+            dateRange.textualDateTime(referenceDateTime: DateTime(2023)),
+            'December 1, 2022 09:30–21:30',
+          );
+          expect(
+            dateRange.textualDateTime(referenceDateTime: DateTime(2023)),
+            dateRange.textualDateTime(referenceDateTime: DateTime(2021)),
+          );
+          expect(
+            dateRange.textualDateTime(
+              referenceDateTime: DateTime(2022),
+              monthDayFormat: (format) => format.add_d().add_LLL(),
+              timeFormat: (format) => format.add_Hms(),
+            ),
+            '1 Dec 09:30:00–21:30:00',
+          );
+          expect(
+            dateRange.textualDateTime(
+              referenceDateTime: DateTime(2022),
+              monthDayFormat: (format) => format.add_MMMMEEEEd(),
+            ),
+            'Thursday, December 1 09:30–21:30',
+          );
+          expect(
+            dateRange.textualDateTime(
+              referenceDateTime: DateTime(2023),
+              fullDateFormat: (format) => format.add_yMMMMEEEEd(),
+            ),
+            'Thursday, December 1, 2022 09:30–21:30',
+          );
+        },
+      );
+
+      test(
+        'should return a textual range representation of a finite '
+        'DateRanger with different years',
+        () {
+          final dateRange = DateRange(
+            startDate: DateTime(2022, 12, 1, 9, 30),
+            endDate: DateTime(2023, 1, 12, 21, 30),
+          );
+          expect(
+            dateRange.textualDateTime(referenceDateTime: DateTime(2022)),
+            'December 1, 2022 09:30 – January 12, 2023 21:30',
+          );
+          expect(
+            dateRange.textualDateTime(
+              fullDateFormat: (format) => format.add_yMd(),
+            ),
+            '12/1/2022 09:30 – 1/12/2023 21:30',
+          );
+          expect(
+            dateRange.textualDateTime(
+              fullDateFormat: (format) => format.add_yMMMMEEEEd(),
+            ),
+            'Thursday, December 1, 2022 09:30 – '
+            'Thursday, January 12, 2023 21:30',
+          );
+        },
+      );
+
+      test(
+        'should return a textual range representation of an infinite '
+        'DateRanger',
+        () {
+          expect(DateRange.infinite.textualDateTime(), 'null – null');
+
+          final startDateRange = DateRange(
+            startDate: DateTime(2022, 12, 1, 9, 30),
+          );
+          expect(
+            startDateRange.textualDateTime(),
+            'December 1, 2022 09:30 – null',
+          );
+
+          final endDateRange = DateRange(
+            endDate: DateTime(2022, 12, 31, 21, 30),
+          );
+          expect(
+            endDateRange.textualDateTime(),
+            'null – December 31, 2022 21:30',
+          );
+        },
+      );
+    });
+
+    group('.textualTime', () {
+      test('should return the textual time range of a finite DateRanger', () {
+        final dateRange = DateRange(
+          startDate: DateTime(2022, 12, 1, 9, 30),
+          endDate: DateTime(2022, 12, 1, 21, 30),
+        );
+        expect(dateRange.textualTime, '09:30–21:30');
+
+        final multiYearDateRange = DateRange(
+          startDate: DateTime(2022, 12, 1, 9, 30),
+          endDate: DateTime(2023, 1, 12, 21, 30),
+        );
+        expect(multiYearDateRange.textualTime, '09:30–21:30');
+      });
+
+      test(
+        'should return the textual local time range of a UTC DateRanger',
+        () {
+          final dateRange = DateRange(
+            startDate: DateTime.utc(2022, 12, 1, 9, 30),
+            endDate: DateTime.utc(2022, 12, 1, 21, 45),
+          );
+          final startDate = dateRange.startDate!.toLocal();
+          final endDate = dateRange.endDate!.toLocal();
+          String padTime(int time) => '$time'.padLeft(2, '0');
+          expect(
+            dateRange.textualTime,
+            '${padTime(startDate.hour)}:${padTime(startDate.minute)}–'
+            '${padTime(endDate.hour)}:${padTime(endDate.minute)}',
+          );
+        },
+      );
+
+      test(
+        'should return the textual time range of an infinite DateRanger',
+        () {
+          expect(DateRange.infinite.textualTime, 'null – null');
+          final dateRange = DateRange(
+            startDate: DateTime(2022, 12, 1, 9, 30),
+          );
+          expect(dateRange.textualTime, '09:30 – null');
+
+          final multiYearDateRange = DateRange(
+            endDate: DateTime(2023, 1, 12, 21, 30),
+          );
+          expect(multiYearDateRange.textualTime, 'null – 21:30');
+        },
+      );
+    });
+
     group('.hoursSpan', () {
       test('should return a Map of the time span of this DateRanger', () {
         final dateRange = DateRange(

test/utils/time_of_day_extension_test.dart
@@ -188,14 +188,6 @@ void main() {
       });
     });
 
-    group('.format24Hour()', () {
-      test('should return a TimeOfDay in 24 hour format', () {
-        expect(const TimeOfDay(hour: 0, minute: 0).format24Hour(), '00:00');
-        expect(const TimeOfDay(hour: 9, minute: 41).format24Hour(), '09:41');
-        expect(const TimeOfDay(hour: 17, minute: 45).format24Hour(), '17:45');
-      });
-    });
-
     // TODO(albertms10): remove when implemented in Flutter, https://github.com/flutter/flutter/pull/59981.
     group('Comparable<TimeOfDay>', () {
       test('.compareTo()', () {

commit d343c102ae32b475fff060af550067fbfb8660ac

fix(booking): call `textualDateTime` method on `toString`
Addresses an issue introduced in #168

lib/src/model/booking/booking.dart
@@ -78,7 +78,7 @@ abstract class Booking extends DateRangeItem {
 
   @override
   String toString() =>
-      [if (isLocked) '🔒', description, textualDateTime].join(' ');
+      [if (isLocked) '🔒', description, textualDateTime()].join(' ');
 
   @override
   bool operator ==(Object other) =>