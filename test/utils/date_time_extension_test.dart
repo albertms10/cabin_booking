08832f01ef43d5ff1d034344f0c638f60a4e7270
Albert Mañosa <albertmasa2@gmail.com>
2023-01-27 20:44:21 +0100

    test(date_time_extension): add test cases for `isToday`-like methods (#195)

--- test/utils/date_time_extension_test.dart
+++ test/utils/date_time_extension_test.dart
@@ -94,5 +94,29 @@ void main() {
         expect(DateTime(2021, 1, 10, 9, 30).toDouble(), closeTo(18637.3, 0.1));
       });
     });
+
+    group('.isToday', () {
+      test('should return whether this DateTime is today', () {
+        final dateTime = DateTime(1970, 1, 10, 9, 30);
+        expect(dateTime.isToday, isFalse);
+        expect(DateTime.now().isToday, isTrue);
+      });
+    });
+
+    group('.isYesterday', () {
+      test('should return whether this DateTime is yesterday', () {
+        expect(DateTime.now().isYesterday, isFalse);
+        final dateTime = DateTime.now().subtract(const Duration(days: 1));
+        expect(dateTime.isYesterday, isTrue);
+      });
+    });
+
+    group('.isTomorrow', () {
+      test('should return whether this DateTime is tomorrow', () {
+        expect(DateTime.now().isTomorrow, isFalse);
+        final dateTime = DateTime.now().add(const Duration(days: 1));
+        expect(dateTime.isTomorrow, isTrue);
+      });
+    });
   });
 }
